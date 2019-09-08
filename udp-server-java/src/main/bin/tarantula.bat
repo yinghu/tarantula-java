27882bf7f22964da70f2f5beffee0211ca2d923c
yinghulu <yinghu.lu@gmail.com>
2019-09-08 03:17:27 -0700

    java UDP server init

---
+++ udp-server-java/src/main/bin/tarantula.bat
@@ -0,0 +1,10 @@
+@echo off
+set JVM_ARGS=-server -XX:MaxDirectMemorySize=64M -Xms512M -Xmx1024M
+setLocal EnableDelayedExpansion
+set CLASSPATH="
+for /R ../lib %%a in (*.jar) do (
+   set CLASSPATH=!CLASSPATH!;%%a
+)
+set CLASSPATH=!CLASSPATH!"
+java %JVM_ARGS% -classpath %CLASSPATH% com.tarantula.integration.udp.Main
+
--- udp-server-java/src/main/bin/tarantula.sh
+++ udp-server-java/src/main/bin/tarantula.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-echo "Starting Tarantula UDP Integration Service ..."
+echo "Starting Tarantula Distribution System  ..."
 find ../lib -name *.jar > lib.txt
+CP="."
 while read line
 do
 V=`echo "${line}"`
 CP=${CP}:$V
 done < lib.txt
 rm -f lib.txt
-JAVA_OPTS="$JAVA_OPTS -Xms512m -Xmx1024m -server"
+JAVA_OPTS="$JAVA_OPTS -Xms512m -Xmx1024m -XX:MaxDirectMemorySize=64M -server"
 java $JAVA_OPTS -classpath $CP com.tarantula.integration.udp.Main
 
 
