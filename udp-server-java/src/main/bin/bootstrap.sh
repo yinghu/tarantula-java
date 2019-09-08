a62868584e372b439d7cd6264070cefae1db1da2
yinghulu <yinghu.lu@gmail.com>
2019-09-08 03:05:34 -0700

    java UDP server init

--- udp-server-java/src/main/bin/bootstrap.sh
+++ udp-server-java/src/main/bin/bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo "Starting Tarantula Distribution System Load Test ..."
+echo "Starting Tarantula UDP Integration Service ..."
 find ../lib -name *.jar > lib.txt
-CP="../conf:../deploy:../../../../test-classes"
 while read line
 do
 V=`echo "${line}"`
@@ -9,7 +8,6 @@ CP=${CP}:$V
 done < lib.txt
 rm -f lib.txt
 JAVA_OPTS="$JAVA_OPTS -Xms512m -Xmx1024m -server"
-echo "16384 65535" > /proc/sys/net/ipv4/ip_local_port_range
-java $JAVA_OPTS -classpath $CP Main $1 $2 $3 $4
+java $JAVA_OPTS -classpath $CP com.tarantula.integration.udp.Main
 
 
--- udp-server-java/src/main/java/com/tarantula/integration/udp/OnPayload.java
+++
@@ -1,7 +0,0 @@
-package com.tarantula.integration.udp;
-
-import com.google.gson.JsonObject;
-
-public interface OnPayload {
-    void on(JsonObject jsonObject);
-}
