370b675b1916772799815cf9fe0426156eaca2d7
yinghulu <yinghu.lu@gmail.com>
2019-11-17 10:09:33 -0800

    added demo files

---
+++ client-integration-unity3d/Assets/Scripts/Demo/Integration.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+using Tarantula.Networking;
+
+public class Integration : MonoBehaviour{
+    
+    public string GEC_HOST;
+    
+    public GameEngineCluster gec;
+    
+    
+    private static Integration instance;
+
+	public static Integration Instance{
+		get
+		{
+			if (instance == null)
+			{
+				instance = FindObjectOfType<Integration>();
+				if (instance == null)
+				{
+					GameObject obj = new GameObject
+					{
+						name = typeof(Integration).Name
+					};
+					instance = obj.AddComponent<Integration>();
+				}
+			}
+
+			return instance;
+		}
+	}
+    
+    
+    void Awake(){
+         gec = new GameEngineCluster(GEC_HOST);
+         DontDestroyOnLoad(this.gameObject);
+    }
+    void Start(){
+           
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    public async void profile(){
+        bool suc = await gec.Profile(this);
+        Debug.Log(gec.profile.nickname);
+    }
+
+}
---
+++ client-integration-unity3d/Assets/Scripts/Demo/Spin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tarantula.Networking;
+public class Spin : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public GameObject menu;
+    bool _active;
+    void Start()
+    {
+        _active = false;
+        menu.SetActive(false);
+    }
+
+   
+    void Update(){
+        if(_active){
+            transform.Rotate(10,speed,10);
+        }    
+    }
+    public async void OnMouseDown(){
+        _active = true;
+        GameEngineCluster gec = Integration.Instance.gec;
+        gec.OnException += (ex)=>{Debug.Log(ex);};
+        Device device = new Device();
+        device.deviceId = "abc123";
+        bool suc = await gec.Device(this,device);
+        if(suc){
+            Debug.Log(gec.presence.systemId);
+            Debug.Log(gec.presence.ticket);
+            Debug.Log(gec.presence.token);
+            //GameObject ui = GameObject.Find("/UI/Menu");
+            menu.SetActive(true);
+            suc = await gec.OnWebSocket();
+            //to do success
+            //_active = false;
+        }
+    }
+}
