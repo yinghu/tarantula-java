8c7d0f59da74cb26305f1f1eac65779ad3a761c4
yinghulu <yinghu.lu@gmail.com>
2019-12-02 23:44:36 -0800

    updated unity c# API with integration/simulator

--- client-integration-unity3d/Assets/Scripts/Integration/Integration.cs
+++ client-integration-unity3d/Assets/Scripts/Integration/Integration.cs
@@ -10,14 +10,10 @@ using Newtonsoft.Json.Linq;
 public class Integration : MonoBehaviour{
     
     public string GEC_HOST;
-    public Spin spin;
-    public GameEngineCluster gec;
+    public string deviceId;
     
-    
+    private GameEngineCluster gec;
     private static Integration instance;
-    private bool onNotification;
-    private bool started;
-    private Descriptor game;
 
 	public static Integration Instance{
 		get
@@ -39,13 +35,10 @@ public class Integration : MonoBehaviour{
 		}
 	}
     
-    
     void Awake(){
          gec = new GameEngineCluster(GEC_HOST);
          gec.OnException += (ex)=>{
              Debug.Log(ex);
-             spin.OnSpin(false);
-             //gec.Close();
          };
          gec.OnWebSocket += _OnWebSocketMessage;
          gec.OnUDPSocket += _OnUDPSocketMessage;
@@ -70,117 +63,39 @@ public class Integration : MonoBehaviour{
         Debug.Log("udp socket->"+suc);
         await gec.OnUDPSocketMessage();
     }
-    void Start(){
-        onNotification = false;  
-        started = false;
+    async void Start(){
+        Device dev = new Device();
+        dev.deviceId = deviceId;
+        await OnDevice(dev);    
     }
-    void OnDestroy(){
-        gec.Close();            
+    async void OnDestroy(){
+       await gec.Close();           
     }
    
     void Update(){
-        if (Input.GetMouseButtonDown(0)){
-            Vector3 mouse = Input.mousePosition;
-            spin.OnMove(mouse);
-        }
+        //if (Input.GetMouseButtonDown(0)){
+            //Vector3 mouse = Input.mousePosition;
+            //spin.OnMove(mouse);
+        //}
     }
-    public async Task<bool> StartDemo(){
-        if(!started){
-            bool suc = await gec.Index(this);
-            User device = new User();
-            device.login = "root";
-            device.nickname = "xnbbbv";
-            device.password = "root";
-            suc = await gec.Login(this,device);
-            if(!suc){
-                Debug.Log(gec.message);
-            }
-            Debug.Log(suc);
-            
-            if(suc){
-                suc = await gec.OnLobby(this,"robotquest");
-                if(suc){
-                    game = gec.gameList()[0];
-                    suc = await gec.OnPlay(this,"robotquest-service/live",game,(gs)=>{
-                        Debug.Log(game.instanceId);
-                        Debug.Log(game.gameId);
-                    });
-                    if(suc){
-                        spin.OnSpin(suc);
-                        started = true;
-                        //await gec.OnStreaming();  
-                        
-                    }
-                    else{
-                        Debug.Log("ops->"+gec.message);    
-                    }
-                }
-                else{
-                    Debug.Log("ops->"+gec.message);
-                }
-            }
-            return suc;
-        }
-        else{
-            started = false;
-            spin.OnSpin(false);
-            Payload p = new Payload();
-            p.command = "onLeave";
-            p.headers = new Header[]{new Header("test","miss")};
-            await gec.OnInstance(this,game,p,(m)=>{Debug.Log(m);});
-            await gec.Close();
-            return false;
-        }
+    //async local wrappers 
+    public async Task<bool> OnDevice(Device device){
+        return await gec.Device(this,device);
     }
-    
-    public async void profile(){
-        bool suc = await gec.Profile(this,gec.presence.systemId);
-        Debug.Log(gec.profile.nickname);
-        await gec.Level(this);
-        await gec.XP(this,"presence","LoginCount");
-        await gec.LeaderBoard(this,"presence","LoginCount","T");
-        User u = new User();
-        u.login = "abc";
-        u.nickname = "nvnv";
-        OutboundMessage<User> om = new OutboundMessage<User>();
-        om.instanceId = game.gameId;
-        om.query = "onMessage";
-        om.label = "game";
-        om.payload = u;
-        await gec.SendOnUDP(om);
+    public async Task<bool> OnLogin(User user){
+        return await gec.Login(this,user);
     }
-    public async void notification(){
-        onNotification = !onNotification;
-        bool suc = await gec.OnNotification("perfect-notification",onNotification);
-        //Debug.Log(gec.profile.nickname);
-        Payload cmd = new Payload();
-        cmd.command = "abc";
-        //cmd.headers = new Header[]{new Header()}
-        await gec.SendOnService("robotquest-service/live",cmd);
+    public async Task<bool> OnLogout(){
+        return await gec.Logout(this);
     }
-    public async void joo(){
-        //onNotification = !onNotification;
-        //bool suc = await gec.OnNotification("perfect-notification",onNotification);
-        //Debug.Log(gec.profile.nickname);
-        Payload cmd = new Payload();
-        cmd.command = "abc";
-        cmd.headers = new Header[]{new Header("a","1"),new Header("b","2")};
-        await gec.SendOnInstance(game.applicationId,game.instanceId,cmd);
+    public async Task<bool> OnRegister(User user){
+        return await gec.Register(this,user);
     }
-    private bool ParseGame(string json){
-            JObject jo = JObject.Parse(json);
-            bool suc = (bool)jo.SelectToken("successful");
-            if(!suc){
-                //message = (string)jo.SelectToken("message");
-                return suc;
-            }
-            game.instanceId = (string)jo.SelectToken("instanceId");
-            JToken tk = jo.SelectToken("gameObject");
-            //for(int i=0;i<tk.Count;i++){
-                //Descriptor gm = tk[i].ToObject<Descriptor>();
-                //_gameList.Add(gm);
-            //}
-            return true;
-    } 
+    public async Task<bool> OnProfile(){
+        return await gec.Profile(this);
+    }
+    public async Task<bool> OnLevel(){
+        return await gec.Level(this);
+    }
 
 }
