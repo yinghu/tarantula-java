280f6e0aab8e5f6e04790ba2e354b2d9d9e59649
yinghulu <yinghu.lu@gmail.com>
2019-12-23 01:37:17 -0800

    updated code base

--- client-integration-unity3d/Assets/Scripts/Simulator/Board.cs
+++ client-integration-unity3d/Assets/Scripts/Simulator/Board.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Board : MonoBehaviour{
     public GameObject redPlayer;
     public GameObject greenPlayer;
-    public GameObject redDeck;
-    public GameObject greenDeck;
+    public GameObject[] itemList;
     void Start(){
         
     }
@@ -16,12 +15,51 @@ public class Board : MonoBehaviour{
     }
     
     public void OnView(){
-        
-        GameObject r = Instantiate(redPlayer,redDeck.transform.position, Quaternion.identity,redDeck.transform);
+        Vector3 mp = new Vector3(400,720,0);
+        Vector3 vc = Camera.main.ScreenToWorldPoint(mp);
+        vc.z = 1;
+        vc.y = -6f;
+        GameObject r = Instantiate(redPlayer,vc,Quaternion.identity,transform);
         Setup r1 = r.GetComponent<Setup>();
         r1._Setup();
-        GameObject g = Instantiate(greenPlayer,greenDeck.transform.position, Quaternion.identity,greenDeck.transform);
+        mp = new Vector3(400,220,0);
+        vc = Camera.main.ScreenToWorldPoint(mp);
+        vc.z = 20;
+        vc.y = -6f;
+        GameObject g = Instantiate(greenPlayer,vc,Quaternion.identity,transform);
         Setup g1 = g.GetComponent<Setup>();
-        g1._Setup();   
+        g1._Setup();
+        
+    }
+    public void OnViewB(){
+        Vector3 mp = new Vector3(400,720,0);
+        Vector3 vc = Camera.main.ScreenToWorldPoint(mp);
+        //vc.z = vc.z*10;
+        Debug.Log(vc);
+        vc.z = 20;
+        vc.y = -6f;
+        GameObject r = Instantiate(itemList[0],vc,Quaternion.identity,transform);
+        Setup r1 = r.GetComponent<Setup>();
+        r1._Setup();    
+    }
+    public void OnViewC(){
+        Vector3 mp = new Vector3(350,720,0);
+        Vector3 vc = Camera.main.ScreenToWorldPoint(mp);
+        Debug.Log(vc);
+        vc.z = 10;
+        vc.y = -6f;
+        GameObject r = Instantiate(itemList[1],vc,Quaternion.identity,transform);
+        Setup r1 = r.GetComponent<Setup>();
+        r1._Setup(); 
+    }
+    public void OnViewD(){
+        Vector3 mp = new Vector3(150,720,0);
+        Vector3 vc = Camera.main.ScreenToWorldPoint(mp);
+        Debug.Log(vc);
+        vc.z = 15;
+        vc.y = -6f;
+        GameObject r = Instantiate(itemList[2],vc,Quaternion.identity,transform);
+        Setup r1 = r.GetComponent<Setup>();
+        r1._Setup();
     }
 }
--- client-integration-unity3d/Assets/Scripts/Simulator/Setup.cs
+++ client-integration-unity3d/Assets/Scripts/Simulator/Setup.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Setup : MonoBehaviour
-{
-   
+public class Setup : MonoBehaviour{
+    
+    private Rigidbody _rig;
+    
     void Start() {
-        
+        _rig = gameObject.GetComponent<Rigidbody>();   
     }
 
     void Update()
@@ -17,5 +18,24 @@ public class Setup : MonoBehaviour
         Debug.Log("setup ...");
         gameObject.name = "gp1000";
         
+    }
+    private  void OnTriggerEnter(Collider hit){
+        if(gameObject.tag == "gig" && hit.gameObject.tag == "robot"){
+            Debug.Log(hit.gameObject.name+" eating the gig...");
+            Destroy(gameObject);
+        }
+        else if(gameObject.tag == "gig" && hit.gameObject.tag == "fws"){
+            _rig.useGravity = false;
+            _rig.isKinematic = true;
+            //_rig.detectCollisions = false;
+            Debug.Log(gameObject.name+" lading the gig...");
+        }
+        else if(gameObject.tag == "robot" && hit.gameObject.tag == "fws"){
+            //_rig.useGravity = true;
+            _rig.isKinematic = true;
+            gameObject.GetComponent<CharacterController>().enabled = true;
+            //_rig.detectCollisions = false;
+            Debug.Log(gameObject.name+" lading the robot...");
+        }
     }
 }
