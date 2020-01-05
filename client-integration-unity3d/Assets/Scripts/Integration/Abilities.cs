f51a090cfd68b81346bb6135b15a1fa713c56a60
yinghulu <yinghu.lu@gmail.com>
2020-01-04 16:52:46 -0800

    editor ext updted

---
+++ client-integration-unity3d/Assets/Scripts/Integration/Abilities.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+[CreateAssetMenu(fileName = "Abilities", menuName = "Scripts/Abilities", order = 1)]
+public class Abilities : ScriptableObject{
+
+    public enum Rarities{Common,Rare,Epic,Legendary}
+
+    [Header("General")]
+    public float x;
+    public float y;
+    public float z;
+    public Rarities rarity;
+    [Header("UI")]
+    [JsonIgnore]
+    public Sprite CardImage;
+}
