7123ef9f7f7d0544ed0aeea7092477881e95a8af
yingh <yinghu.lu@gmail.com>
2020-05-15 21:47:20 -0700

    platform code refactoring

--- platform/src/main/java/com/tarantula/platform/presence/GameCluster.java
+++ platform/src/main/java/com/tarantula/platform/presence/GameCluster.java
@@ -4,8 +4,10 @@ import com.hazelcast.nio.serialization.Portable;
 import com.hazelcast.nio.serialization.PortableReader;
 import com.hazelcast.nio.serialization.PortableWriter;
 import com.tarantula.platform.DefaultDescriptor;
+import com.tarantula.platform.event.PortableEventRegistry;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class GameCluster extends DefaultDescriptor implements Portable {
 
@@ -17,5 +19,24 @@ public class GameCluster extends DefaultDescriptor implements Portable {
     @Override
     public void readPortable(PortableReader portableReader) throws IOException {
 
+    }
+    @Override
+    public Map<String,Object> toMap(){
+        properties.put("typeId",typeId);
+        //properties.put()
+        return properties;
+    }
+    @Override
+    public void fromMap(Map<String,Object> properties){
+
+    }
+
+    @Override
+    public int getClassId() {
+        return PortableEventRegistry.GAME_CLUSTER_CID;
+    }
+    @Override
+    public int getFactoryId() {
+        return PortableEventRegistry.OID;
     }
 }
