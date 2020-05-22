4150ad3e9742c5c28c60d5f65a5029cf33f85b70
yingh <yinghu.lu@gmail.com>
2020-05-21 23:18:20 -0700

    platform code refactoring

---
+++ platform/src/main/java/com/tarantula/platform/playlist/FriendList.java
@@ -0,0 +1,22 @@
+package com.tarantula.platform.playlist;
+
+import com.tarantula.ApplicationContext;
+import com.tarantula.Module;
+import com.tarantula.Session;
+
+public class FriendList  implements Module {
+    @Override
+    public boolean onRequest(Session session, byte[] payload, OnUpdate update) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void setup(ApplicationContext context) throws Exception {
+
+    }
+
+    @Override
+    public String label() {
+        return "friendList";
+    }
+}
