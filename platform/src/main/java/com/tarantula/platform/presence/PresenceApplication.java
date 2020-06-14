dd5690fec109872411a24bfed6a1a8d6acf1285d
yingh <yinghu.lu@gmail.com>
2020-06-13 22:54:28 -0700

    capacity/duration  support per level to override lobby setting if available on level settings

--- platform/src/main/java/com/tarantula/platform/presence/PresenceApplication.java
+++ platform/src/main/java/com/tarantula/platform/presence/PresenceApplication.java
@@ -65,6 +65,7 @@ public class PresenceApplication extends TarantulaApplicationHeader implements O
             liveGame.lobbyList.add(this.context.lobby(liveGame.name+"-service"));
             liveGame.lobbyList.add(this.context.lobby(liveGame.name+"-data"));
             GameServiceProvider gsp = this.context.serviceProvider(liveGame.name+"-service");
+            ///
             session.write(liveGame.toJson().toString().getBytes(),this.descriptor.responseLabel());
         }
         else if(session.action().equals("onAddEmail")){
