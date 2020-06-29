70dfd0f30090473ec943f535d0676aa7aef0e854
yinghulu <yinghu.lu@gmail.com>
2020-06-28 17:02:27 -0700

    service stop/restart on cluster join/leaving

--- platform/src/main/java/com/tarantula/platform/service/DataStoreProvider.java
+++ platform/src/main/java/com/tarantula/platform/service/DataStoreProvider.java
@@ -11,7 +11,7 @@ import java.util.Map;
  */
 public interface DataStoreProvider extends ServiceProvider {
 
-    int CONCURRENCY_ACCESS_LIMIT = 100;
+    int CONCURRENCY_ACCESS_LIMIT = 8;
 
     void configure(Map<String,String> properties);
     //create none-partitioned integration scope data store
