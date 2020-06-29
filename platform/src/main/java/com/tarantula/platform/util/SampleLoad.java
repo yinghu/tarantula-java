8392e1cde48b4f98eb1d7dec2f8261551fb91cc2
yinghulu <yinghu.lu@gmail.com>
2020-06-29 12:09:08 -0700

    http call on jdk client

--- platform/src/main/java/com/tarantula/platform/util/SampleLoad.java
+++ platform/src/main/java/com/tarantula/platform/util/SampleLoad.java
@@ -4,12 +4,18 @@ import com.google.gson.JsonObject;
 import com.tarantula.Session;
 
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class SampleLoad {
     private final String host;
     private final String prefix;
     private final int size;
     private HttpCaller httpCaller;
+    private ExecutorService pool;
+
     public SampleLoad(String host,String prefix,int size){
         this.host = host;
         this.prefix = prefix!=null?prefix: UUID.randomUUID().toString();
@@ -18,12 +24,14 @@ public class SampleLoad {
     public void _init() throws Exception{
         httpCaller = new HttpCaller(host);
         httpCaller._init();
+        pool = Executors.newFixedThreadPool(50);
     }
-    public void batch(){
+    public void batch() throws Exception{
+        CountDownLatch batch = new CountDownLatch(size);
+        AtomicLong timeRun = new AtomicLong(0);
         for(int i=0;i<size;i++){
             try{
                 //register
-                System.out.println(httpCaller.index());
                 String _login = prefix+"-"+i;
                 String[] headers = new String[]{
                         Session.TARANTULA_TAG,"index/user",
@@ -33,16 +41,31 @@ public class SampleLoad {
                 JsonObject jsonObject = new JsonObject();
                 jsonObject.addProperty("login",_login);
                 jsonObject.addProperty("password","password");
-                System.out.println(httpCaller.post("user/action",jsonObject.toString().getBytes(),headers));
-
+                pool.execute(()->{
+                    try{
+                        long st = System.currentTimeMillis();
+                        httpCaller.index();
+                        httpCaller.post("user/action",jsonObject.toString().getBytes(),headers);
+                        long ed = System.currentTimeMillis()-st;
+                        timeRun.addAndGet(ed);
+                    }catch (Exception ex){
+                        ex.printStackTrace();
+                    }
+                    finally {
+                        batch.countDown();
+                    }
+                });
             }catch (Exception ex){
                 //ignore
                 ex.printStackTrace();
             }
         }
+        batch.await();
+        pool.shutdownNow();
+        System.out.println("Average duration ["+(timeRun.get()/size)+"]");
     }
     public static void main(String[] args) throws Exception{
-        SampleLoad sampleLoad = new SampleLoad("http://10.0.0.234:8090",null,1000);
+        SampleLoad sampleLoad = new SampleLoad("http://10.0.0.234:8090",null,50000);
         sampleLoad._init();
         sampleLoad.batch();
     }
