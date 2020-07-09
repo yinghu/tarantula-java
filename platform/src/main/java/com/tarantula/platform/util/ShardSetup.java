9d597a6c77a879b7beb3c42290a1034915157178
yinghulu <yinghu.lu@gmail.com>
2020-07-08 21:16:24 -0700

    data store enhancement

--- platform/src/main/java/com/tarantula/platform/util/ShardSetup.java
+++ platform/src/main/java/com/tarantula/platform/util/ShardSetup.java
@@ -13,17 +13,17 @@ public class ShardSetup {
 
         Connection connection = DriverManager.getConnection("jdbc:mysql://10.0.0.153:3306?user=tarantula&password=tarantula");
         Statement cmd = connection.createStatement();
-        cmd.execute("CREATE DATABASE IF NOT EXISTS integration0");
-        cmd.execute("CREATE DATABASE IF NOT EXISTS integration1");
-        cmd.execute("CREATE DATABASE IF NOT EXISTS integration");
-        cmd.execute("CREATE DATABASE IF NOT EXISTS tarantula");
-        cmd.execute("USE integration");
+        cmd.execute("CREATE DATABASE IF NOT EXISTS integration10");
+        //cmd.execute("CREATE DATABASE IF NOT EXISTS integration1");
+        //cmd.execute("CREATE DATABASE IF NOT EXISTS integration");
+        cmd.execute("CREATE DATABASE IF NOT EXISTS tarantula10");
+        cmd.execute("USE integration10");
         cmd.execute("CREATE TABLE IF NOT EXISTS meta_info (data_store_name VARCHAR(25) PRIMARY KEY,node VARCHAR(15),bucket VARCHAR(15),scope INT,version INT)");
-        cmd.execute("USE integration1");
-        cmd.execute("CREATE TABLE IF NOT EXISTS meta_info (data_store_name VARCHAR(25) PRIMARY KEY,node VARCHAR(15),bucket VARCHAR(15),scope INT,version INT)");
-        cmd.execute("USE integration2");
-        cmd.execute("CREATE TABLE IF NOT EXISTS meta_info (data_store_name VARCHAR(25) PRIMARY KEY,node VARCHAR(15),bucket VARCHAR(15),scope INT,version INT)");
-        cmd.execute("USE tarantula");
+        //cmd.execute("USE integration1");
+        //cmd.execute("CREATE TABLE IF NOT EXISTS meta_info (data_store_name VARCHAR(25) PRIMARY KEY,node VARCHAR(15),bucket VARCHAR(15),scope INT,version INT)");
+        //cmd.execute("USE integration2");
+        //cmd.execute("CREATE TABLE IF NOT EXISTS meta_info (data_store_name VARCHAR(25) PRIMARY KEY,node VARCHAR(15),bucket VARCHAR(15),scope INT,version INT)");
+        cmd.execute("USE tarantula10");
         cmd.execute("CREATE TABLE IF NOT EXISTS meta_info (data_store_name VARCHAR(25) PRIMARY KEY,node VARCHAR(15),bucket VARCHAR(15),scope INT,version INT)");
 
         cmd.close();
