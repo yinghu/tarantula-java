fb825d5d9d51a3b855a4bb89e4542248537a4fd1
yinghulu <yinghu.lu@gmail.com>
2020-07-15 14:09:57 -0700

    data store enhancement

--- platform/src/main/java/com/tarantula/platform/util/ShardSetup.java
+++ platform/src/main/java/com/tarantula/platform/util/ShardSetup.java
@@ -2,7 +2,6 @@ package com.tarantula.platform.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class ShardSetup {
@@ -12,18 +11,7 @@ public class ShardSetup {
         Connection connection = DriverManager.getConnection("jdbc:mysql://10.0.0.153:3306?user=tarantula&password=tarantula");
         Statement cmd = connection.createStatement();
         cmd.execute("CREATE DATABASE IF NOT EXISTS " + shard);
-        //cmd.execute("USE " + shard);
-        //cmd.execute("CREATE TABLE IF NOT EXISTS meta_info (p INT NOT NULL PRIMARY KEY,n VARCHAR(15),v INT)");
         cmd.close();
-        //PreparedStatement pstm = connection.prepareStatement("INSERT INTO meta_info VALUES(?,?,?)");
-        //for(int i=0;i<partitions;i++){
-            //pstm.setInt(1,i);
-            //pstm.setString(2,"pending");
-            //pstm.setInt(3,0);
-            //pstm.execute();
-            //pstm.clearParameters();
-        //}
-        //pstm.close();
         connection.close();
     }
 
