bf1fcd716ea7249607af9575400a21bae0fffa0b
yinghulu <yinghu.lu@gmail.com>
2020-07-18 13:57:29 -0700

    play-list module init

---
+++ modules/play-list/src/main/java/com/tarantula/playlist/RecentlyPlayListModule.java
@@ -0,0 +1,28 @@
+package com.tarantula.playlist;
+
+import com.tarantula.ApplicationContext;
+import com.tarantula.Module;
+import com.tarantula.OnLog;
+import com.tarantula.Session;
+
+public class RecentlyPlayListModule implements Module {
+
+    private ApplicationContext context;
+
+    @Override
+    public boolean onRequest(Session session, byte[] payload, OnUpdate onUpdate) throws Exception {
+        session.write(payload,label());
+        return false;
+    }
+
+    @Override
+    public void setup(ApplicationContext applicationContext) throws Exception {
+        this.context = applicationContext;
+        this.context.log("Recently Play List started", OnLog.WARN);
+    }
+
+    @Override
+    public String label() {
+        return "playlist";
+    }
+}
