102d351928446fb6cbecb7ecedd7d0c6e12cf143
yinghulu <yinghu.lu@gmail.com>
2020-09-29 15:36:30 -0700

    game server integration enhancement

--- udp-server-java/src/main/java/com/icodesoftware/integration/Main.java
+++ udp-server-java/src/main/java/com/icodesoftware/integration/Main.java
@@ -1,16 +1,55 @@
 package com.icodesoftware.integration;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.icodesoftware.integration.udp.UDPReceiver;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.logging.LogManager;
+
 public class Main {
+    static {
+        try {
+            Properties _props = new Properties();
+            _props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("logging.properties"));
+            String[] fid = _props.getProperty("java.util.logging.FileHandler.pattern").split("/");
+            StringBuilder fs = new StringBuilder();
+            fs.append(System.getProperty("user.home")).append(FileSystems.getDefault().getSeparator());
+            for(int i=1;i<fid.length-1;i++){
+                fs.append(fid[i]).append(FileSystems.getDefault().getSeparator());
+            }
+            Path _path = Paths.get(fs.substring(0,fs.length()-1));
+            if(!Files.exists(_path)){
+                Files.createDirectories(_path);
+            }
+            LogManager.getLogManager().readConfiguration(Thread.currentThread().getContextClassLoader().getResourceAsStream("logging.properties"));
+        }catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
     public static void main(String[] args) throws Exception{
-        UDPReceiver udpReceiver = new UDPReceiver("10.0.0.192",16393);
+        JsonParser jsonParser = new JsonParser();
+        File f = new File("/etc/tarantula/udp.conf");
+        JsonObject config;
+        if(f.exists()){
+            config = jsonParser.parse(new InputStreamReader(new FileInputStream(f))).getAsJsonObject();
+        }
+        else{
+            config = jsonParser.parse(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("udp.conf"))).getAsJsonObject();
+        }
+        UDPReceiver udpReceiver = new UDPReceiver(config);
         udpReceiver.start();
         Thread t = new Thread(udpReceiver);
         t.start();
-        System.out.println("UDP STARTED");
-        t.join();
-        udpReceiver.stop();
-        System.out.println("UDP STOPPED");
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            try{udpReceiver.stop();}catch (Exception ex){}
+        }));
     }
 }
