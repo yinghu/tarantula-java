428f9775e204e1fb1fe787c4d4ac9c8f50509ecb
yinghulu <yinghu.lu@gmail.com>
2020-11-05 10:50:12 -0800

    move use case updates

--- unity-integration/Assets/Integration/Move/Player.cs
+++ unity-integration/Assets/Integration/Move/Player.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Concurrent;
+using GameClustering;
+using UnityEngine;
 
 namespace Integration.Move
 {
@@ -7,21 +10,41 @@ namespace Integration.Move
         private Vector3 _target;
         private const float Speed = 3f;
         private Vector3 _end;
-
+        private ConcurrentQueue<Vector3> _queue;
+        private IntegrationManager _integrationManager;
         private void Start()
         {
             _target = transform.position;
             _end = _target;
+            _queue = new ConcurrentQueue<Vector3>();
+            _integrationManager = IntegrationManager.Instance;
+            _integrationManager.Messenger.RegisterMessageHandler(MessageType.Relay,1, (sessionId, buffer) =>
+            {
+                _queue.Enqueue(buffer.GetVector3());
+            });
         }
 
-        public void Move(Vector3 target)
+        public async void Move(Vector3 target)
         {
             _end = target;
+            using (var buffer = new DataBuffer())
+            {
+                buffer.PutVector3(target);
+                await _integrationManager.Messenger.SendAsync(MessageType.Relay, 1, false, buffer);
+            }
+        }
+
+        private void Update()
+        {
+            if (_queue.TryDequeue(out var pos))
+            {
+                _end = pos;
+            }
         }
 
         private void FixedUpdate()
         {
-            transform.position = Vector3.Lerp(transform.position, _end, Speed*Time.fixedDeltaTime);       
+            transform.position = Vector3.Lerp(transform.position, _end, Speed*Time.fixedDeltaTime);
         }
     }
     
