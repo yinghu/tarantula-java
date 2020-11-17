49c37e9bd89f487cf79c46c3cc0e3590a61bdaf9
yinghulu <yinghu.lu@gmail.com>
2020-11-16 21:50:56 -0800

    code updated

--- unity-integration/Assets/Integration/Game/FreeMove.cs
+++ unity-integration/Assets/Integration/Game/FreeMove.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
 using GameClustering;
 using UnityEngine;
 
@@ -12,8 +13,10 @@ namespace Integration.Game
         private Vector3 _end;
         private ConcurrentQueue<Vector3> _queue;
         private IntegrationManager _integrationManager;
+        private float _timer;
         private void Start()
         {
+            _timer = 0.5f;
             _target = transform.position;
             _end = _target;
             _queue = new ConcurrentQueue<Vector3>();
@@ -24,7 +27,7 @@ namespace Integration.Game
             });
         }
 
-        public async void Move(Vector3 target)
+        private async Task Move(Vector3 target)
         {
             _end = target;
             using (var buffer = new DataBuffer())
@@ -41,9 +44,22 @@ namespace Integration.Game
                 _end = pos;
             }
         }
-        private void FixedUpdate()
+        private async void FixedUpdate()
         {
             transform.position = Vector3.Lerp(transform.position, _end, Speed*Time.fixedDeltaTime);
+            _timer -= Time.fixedDeltaTime;
+            if (_timer > 0)
+            {
+                return;
+            }
+            _timer = 0.5f;
+            var cur = transform.position;
+            var left = new Vector3(cur.x+1,cur.y,cur.z);
+            await Move(left);
+        }
+        private void OnCollisionEnter(Collision other)
+        {
+            Debug.Log(other.gameObject.tag);
         }
     }
 }
