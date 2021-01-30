8e0646429506393bbfa575002a4862118f12ece4
yinghu lu <yinghu.lu@gmail.com>
2021-01-30 00:08:06 -0800

    enabled maven for protocol module

--- platform/src/main/java/com/tarantula/cci/DevelopmentEventHandler.java
+++ platform/src/main/java/com/tarantula/cci/DevelopmentEventHandler.java
@@ -1,37 +1,18 @@
 package com.tarantula.cci;
 
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.icodesoftware.*;
 import com.icodesoftware.logging.JDKLogger;
 import com.icodesoftware.service.*;
-import com.tarantula.platform.ClientConnection;
-import com.tarantula.platform.ResponseHeader;
-import com.tarantula.platform.UniverseConnection;
 import com.tarantula.platform.event.ResponsiveEvent;
-import com.tarantula.platform.event.ServerPushEvent;
-import com.tarantula.platform.util.ConnectionDeserializer;
-import com.tarantula.platform.util.ConnectionSerializer;
-import com.tarantula.platform.util.ResponseSerializer;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-
 
 public class DevelopmentEventHandler implements RequestHandler {
 
     private static TarantulaLogger log = JDKLogger.getLogger(DevelopmentEventHandler.class);
-
-    private EventService eventService;
     private TokenValidatorProvider tokenValidatorProvider;
-
-    private String serverTopic;
-    private final ConcurrentHashMap<String,OnExchange> _hex = new ConcurrentHashMap<>();
-    private GsonBuilder builder;
+    private String homeDir;
 
     public String name(){
         return "/development";
@@ -44,10 +25,9 @@ public class DevelopmentEventHandler implements RequestHandler {
             if(typeId==null){
                 throw new RuntimeException("Illegal access");
             }
-            String _homeDir = System.getProperty("user.home");
-            String _file = exchange.path().replaceFirst("/development",_homeDir);
-            log.warn(_file);
-            InputStream inputStream = new FileInputStream(new File(_file));
+            String _file = exchange.path().replaceFirst("/development","");
+            log.warn("Downloading from developer->"+typeId);
+            InputStream inputStream = new FileInputStream(new File(homeDir+_file));
             byte[] _payload = inputStream.readAllBytes();
             inputStream.close();
             ResponsiveEvent responsiveEvent = new ResponsiveEvent("","",_payload,"start",true);
@@ -55,20 +35,21 @@ public class DevelopmentEventHandler implements RequestHandler {
 
         }catch (Exception ex){
             ex.printStackTrace();
-            _hex.remove(exchange.id()); //removed cache on any errors
             exchange.onError(ex,ex.getMessage());
         }
     }
 
     @Override
     public void start() throws Exception {
-        this.builder = new GsonBuilder();
-        this.builder.registerTypeAdapter(ResponseHeader.class,new ResponseSerializer());
-        this.builder.registerTypeAdapter(UniverseConnection.class,new ConnectionSerializer());
-        this.builder.registerTypeAdapter(Connection.class,new ConnectionDeserializer());
-        this.serverTopic = UUID.randomUUID().toString();
-        this.eventService.registerEventListener(this.serverTopic,this);
-        log.info("Development event handler started->"+System.getProperty("user.home"));
+        String _homeDir = System.getProperty("user.home");
+        int wc = _homeDir.indexOf(":");
+        if(wc>0){
+            homeDir = _homeDir.substring(wc+1).replace('\\','/')+"/.m2/repository";
+        }
+        else{
+            homeDir = _homeDir+"/.m2/repository";
+        }
+        log.info("Development event handler started->"+homeDir);
     }
 
     @Override
@@ -77,19 +58,10 @@ public class DevelopmentEventHandler implements RequestHandler {
     }
 
     public void setup(ServiceContext tcx){
-        this.eventService = tcx.eventService(Distributable.INTEGRATION_SCOPE);
         this.tokenValidatorProvider = (TokenValidatorProvider) tcx.serviceProvider(TokenValidatorProvider.NAME);
     }
     public  boolean onEvent(Event event){
-        OnExchange hx = this._hex.get(event.sessionId());
-        if(hx!=null){
-           if(hx.onEvent(event)){ //remove on true marked as closed connect or session
-               _hex.remove(event.sessionId());
-           }
-        }
-        else{
-           log.warn(event.toString()+" unexpected removed on server push");
-        }
+
         return true;
     }
     public void onCheck(){
