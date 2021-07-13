9a1076300404fd06653b4a763c21e03b04d9111e
yinghu lu <yinghu.lu@gmail.com>
2021-07-13 00:27:38 -0700

    game zone reformat

--- platform/src/main/java/com/tarantula/game/service/PVPJoinProxy.java
+++ platform/src/main/java/com/tarantula/game/service/PVPJoinProxy.java
@@ -9,6 +9,12 @@ public class PVPJoinProxy implements GameZone.JoinProxy {
 
     @Override
     public Stub join(Arena arena, Rating rating) {
-        return null;
+        Stub stub = new Stub();
+        stub.successful(true);
+        stub.seat = rating.xpLevel;
+        stub.rating = rating;
+        stub.arena = arena.name();
+        stub.owner(rating.distributionKey());
+        return stub;
     }
 }
