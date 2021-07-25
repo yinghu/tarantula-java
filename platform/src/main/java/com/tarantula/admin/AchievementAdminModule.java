f48946ed25c41c187294b409a43b8ffdce8520ef
yinghu lu <yinghu.lu@gmail.com>
2021-07-25 00:58:37 -0700

    achievement module hook

---
+++ platform/src/main/java/com/tarantula/admin/AchievementAdminModule.java
@@ -0,0 +1,39 @@
+package com.tarantula.admin;
+
+import com.icodesoftware.ApplicationContext;
+import com.icodesoftware.Module;
+import com.icodesoftware.OnLog;
+import com.icodesoftware.Session;
+import com.icodesoftware.service.DeploymentServiceProvider;
+import com.icodesoftware.util.JsonUtil;
+import com.tarantula.game.service.GameServiceProvider;
+import com.tarantula.platform.ApplicationConfiguration;
+import com.tarantula.platform.GameCluster;
+
+public class AchievementAdminModule implements Module {
+    private ApplicationContext context;
+    private DeploymentServiceProvider deploymentServiceProvider;
+    @Override
+    public boolean onRequest(Session session, byte[] bytes, OnUpdate onUpdate) throws Exception {
+        if (session.action().equals("onSave")){
+            GameCluster gameCluster = this.deploymentServiceProvider.gameCluster(session.name());
+            String serviceName = (String)gameCluster.property(GameCluster.GAME_SERVICE);
+            GameServiceProvider gameServiceProvider = this.context.serviceProvider(serviceName);
+            ApplicationConfiguration app = new ApplicationConfiguration();
+            app.configurationType(this.context.descriptor().category());
+            gameServiceProvider.register(app);
+            session.write(JsonUtil.toSimpleResponse(true,serviceName).getBytes());
+        }
+        else {
+            throw new UnsupportedOperationException(session.action()+" not supported");
+        }
+        return false;
+    }
+
+    @Override
+    public void setup(ApplicationContext applicationContext) throws Exception {
+        this.context = applicationContext;
+        this.deploymentServiceProvider = context.serviceProvider(DeploymentServiceProvider.NAME);
+        this.context.log("Achievement admin module started", OnLog.WARN);
+    }
+}
