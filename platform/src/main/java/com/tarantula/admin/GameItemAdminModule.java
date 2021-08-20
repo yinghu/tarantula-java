39103dc0a47224d94e84c0b05d7d77909f3afb35
yingh <yinghu.lu@gmail.com>
2021-08-20 16:38:52 -0700

    more service hooked on game cluster create

---
+++ platform/src/main/java/com/tarantula/admin/GameItemAdminModule.java
@@ -0,0 +1,84 @@
+package com.tarantula.admin;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.icodesoftware.Module;
+import com.icodesoftware.*;
+import com.icodesoftware.service.DeploymentServiceProvider;
+import com.icodesoftware.util.JsonUtil;
+import com.tarantula.game.service.GameServiceProvider;
+import com.tarantula.platform.GameCluster;
+import com.tarantula.platform.item.Item;
+import com.tarantula.platform.item.ItemQuery;
+import com.tarantula.platform.service.ApplicationPreSetup;
+import com.tarantula.platform.util.SystemUtil;
+
+import java.util.List;
+
+public class GameItemAdminModule implements Module {
+    private ApplicationContext context;
+    private DeploymentServiceProvider deploymentServiceProvider;
+    @Override
+    public boolean onRequest(Session session, byte[] payload, OnUpdate onUpdate) throws Exception {
+        if(session.action().equals("onList")){
+            GameCluster gameCluster = this.deploymentServiceProvider.gameCluster(session.name());
+            Descriptor app = this.loadDescriptor(gameCluster,this.context.descriptor().category());
+            ApplicationPreSetup preSetup = SystemUtil.applicationPreSetup((String) gameCluster.property(GameCluster.LOBBY_PRE_SETUP_NAME));
+            List<Item> items = preSetup.list(this.context,app,new ItemQuery());
+            session.write(toJson(items).toString().getBytes());
+        }
+        else if (session.action().equals("onSave")){
+            GameCluster gameCluster = this.deploymentServiceProvider.gameCluster(session.name());
+            Item app = new Item();
+            if(app.configureAndValidate(payload)){
+                app.configurationType(this.context.descriptor().category());
+                app.configurationName("gem");
+                app.configurationCategory("item");
+                Descriptor desc = loadDescriptor(gameCluster,this.context.descriptor().category());
+                SystemUtil.applicationPreSetup((String) gameCluster.property(GameCluster.LOBBY_PRE_SETUP_NAME)).save(this.context,desc,app);
+                GameServiceProvider gameServiceProvider = this.context.serviceProvider((String) gameCluster.property(GameCluster.GAME_SERVICE));
+                gameServiceProvider.configurationServiceProvider().register(app);
+                session.write(JsonUtil.toSimpleResponse(true,app.distributionKey()).getBytes());
+            }
+            else{
+                session.write(JsonUtil.toSimpleResponse(false,"failed to save item").getBytes());
+            }
+        }
+        else if(session.action().equals("onLoad")){
+
+        }
+
+        else {
+            throw new UnsupportedOperationException(session.action()+" not supported");
+        }
+        return false;
+    }
+
+    @Override
+    public void setup(ApplicationContext applicationContext) throws Exception {
+        this.context = applicationContext;
+        this.deploymentServiceProvider = context.serviceProvider(DeploymentServiceProvider.NAME);
+        this.context.log("game item admin module started", OnLog.WARN);
+    }
+    private Descriptor loadDescriptor(GameCluster gameCluster, String category){
+        String lobbyTypeId = (String)gameCluster.property(GameCluster.GAME_SERVICE);
+        Lobby lobby = this.deploymentServiceProvider.lobby(lobbyTypeId);
+        Descriptor[] descriptors = {null};
+        lobby.entryList().forEach((d)->{
+            if(d.category().equals(category)){
+                descriptors[0]=d;
+            }
+        });
+        return descriptors[0];
+    }
+    private JsonObject toJson(List<Item> itemList){
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("successful",true);
+        JsonArray alist = new JsonArray();
+        itemList.forEach((v)->{
+            alist.add(v.toJson());
+        });
+        jsonObject.add("itemList",alist);
+        return jsonObject;
+    }
+}
---
+++ platform/src/main/java/com/tarantula/game/module/DailyGiveAwayModule.java
@@ -0,0 +1,26 @@
+package com.tarantula.game.module;
+
+import com.icodesoftware.ApplicationContext;
+import com.icodesoftware.Module;
+import com.icodesoftware.OnLog;
+import com.icodesoftware.Session;
+
+public class DailyGiveAwayModule implements Module {
+    private ApplicationContext context;
+
+    @Override
+    public boolean onRequest(Session session, byte[] bytes, OnUpdate onUpdate) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void setup(ApplicationContext applicationContext) throws Exception {
+        this.context = applicationContext;
+        this.context.log("daily giveaway module started", OnLog.WARN);
+    }
+    @Override
+    public void clear(){
+
+    }
+
+}
