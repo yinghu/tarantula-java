8f0e08c2673d93078dc74302ca0145c02fc2a09d
Ying Hu Lu <yinghu@perfectday.games>
2021-08-27 20:47:35 -0700

    item api reformat

---
+++ platform/src/main/java/com/tarantula/platform/item/Commodity.java
@@ -0,0 +1,7 @@
+package com.tarantula.platform.item;
+
+import com.icodesoftware.Configuration;
+import com.icodesoftware.util.RecoverableObject;
+
+public class Commodity extends RecoverableObject implements Configuration {
+}
