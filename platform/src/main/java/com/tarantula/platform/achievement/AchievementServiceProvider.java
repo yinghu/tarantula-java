53dc19dd5841e373089e3556195af8e047388959
Ying Hu Lu <yinghu@perfectday.games>
2021-09-19 23:36:37 -0700

    achievement service impl

---
+++ platform/src/main/java/com/tarantula/platform/achievement/AchievementServiceProvider.java
@@ -0,0 +1,21 @@
+package com.tarantula.platform.achievement;
+
+import com.icodesoftware.service.ServiceProvider;
+
+public class AchievementServiceProvider implements ServiceProvider {
+
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public void start() throws Exception {
+
+    }
+
+    @Override
+    public void shutdown() throws Exception {
+
+    }
+}
