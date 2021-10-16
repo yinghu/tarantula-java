dc7e5c38a03a39cbc5a45065ddff188e565080d0
yingh <yinghu.lu@gmail.com>
2021-10-15 22:38:27 -0700

    udp prediction replication

--- integration-server/src/main/java/com/icodesoftware/integration/udp/RelayService.java
+++ integration-server/src/main/java/com/icodesoftware/integration/udp/RelayService.java
@@ -2,12 +2,15 @@ package com.icodesoftware.integration.udp;
 
 import com.icodesoftware.TarantulaLogger;
 import com.icodesoftware.logging.JDKLogger;
-import com.icodesoftware.protocol.OutboundMessage;
 
+
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.util.Arrays;
+import java.util.HashSet;
 
 
 public class RelayService implements Runnable{
@@ -16,10 +19,11 @@ public class RelayService implements Runnable{
 
 
     private DatagramSocket datagramChannel;
+    private HashSet<SocketAddress> socketAddresses = new HashSet<>();
 
     public void start() throws Exception{
         this.datagramChannel = new DatagramSocket(null);
-        InetSocketAddress addr = new InetSocketAddress("10.0.0.153",11933);
+        InetSocketAddress addr = new InetSocketAddress("10.0.0.192",11933);
         this.datagramChannel.bind(addr);
     }
     public void shutdown() throws Exception{
@@ -35,8 +39,16 @@ public class RelayService implements Runnable{
             try{
                 DatagramPacket buffer = new DatagramPacket(new byte[512],512);
                 this.datagramChannel.receive(buffer);
+                socketAddresses.add(buffer.getSocketAddress());
                 byte[] payload = Arrays.copyOf(buffer.getData(),buffer.getLength());
-                this.datagramChannel.send(new DatagramPacket(payload,payload.length,buffer.getSocketAddress()));
+                socketAddresses.forEach((s)->{
+                    try{
+                        this.datagramChannel.send(new DatagramPacket(payload, payload.length,s));
+                    }
+                    catch (IOException ioex){
+                        ioex.printStackTrace();
+                    }
+                });
                 //mQueue.offer(new PendingMessage(payload,buffer.getSocketAddress()));
             }catch (Exception ex){
                 //ignore
