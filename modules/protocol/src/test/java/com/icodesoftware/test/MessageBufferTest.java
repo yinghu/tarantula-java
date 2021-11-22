b7ffb48fd10ed5614435cb4dae2eb4c169545a6f
yingh <yinghu.lu@gmail.com>
2021-11-21 21:52:24 -0800

    bit opts

--- modules/protocol/src/test/java/com/icodesoftware/test/MessageBufferTest.java
+++ modules/protocol/src/test/java/com/icodesoftware/test/MessageBufferTest.java
@@ -5,6 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.AnnotatedArrayType;
+
 
 public class MessageBufferTest {
 
@@ -51,6 +53,31 @@ public class MessageBufferTest {
         messageBuffer.readHeader();
         messageBuffer.rewind();
         Assert.assertEquals(messageBuffer.toArray().length,50);
+    }
+    @Test(groups = { "message buffer" })
+    public void bitwiseMessageBufferTest(){
+        byte b = (byte)0x00;
+        Assert.assertEquals(b,0);
+        Assert.assertEquals(b&1,0);
+        Assert.assertEquals(b&2,0);
+        Assert.assertEquals(b&4,0);
 
+        int x = b|0x04; //0000,0100 -> broadcasting
+        Assert.assertEquals(x,4);
+        Assert.assertEquals(x&1,0);
+        Assert.assertEquals(x&2,0);
+        Assert.assertEquals(x&4,4);
+
+        int y = x|0x01; //0000,0001 -> ack
+        Assert.assertEquals(y,5);
+        Assert.assertEquals(y&1,1);
+        Assert.assertEquals(y&2,0);
+        Assert.assertEquals(y&4,4);
+
+        int z = y|0x02; //0000,0010 -> encrypted
+        Assert.assertEquals(z,7);
+        Assert.assertEquals(z&1,1);
+        Assert.assertEquals(z&2,2);
+        Assert.assertEquals(z&4,4);
     }
 }
