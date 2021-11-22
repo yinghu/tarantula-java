9ee290d9bab48af718f58d666d0bd981a6070c27
yingh <yinghu.lu@gmail.com>
2021-11-22 15:23:22 -0800

    achievemnt command hook

--- platform/src/main/java/com/tarantula/game/service/ServiceCommand.java
+++ platform/src/main/java/com/tarantula/game/service/ServiceCommand.java
@@ -8,4 +8,7 @@ public class ServiceCommand {
     public final static short REQUEST_TOURNAMENT_LEADERBOARD = 3;
     public final static short COMMIT_TOURNAMENT_SCORE = 4;
 
+    public final static short REQUEST_ACHIEVEMENT_LIST = 5;
+    public final static short COMMIT_ACHIEVEMENT = 6;
+
 }
