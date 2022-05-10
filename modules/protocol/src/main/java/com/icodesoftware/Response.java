1a4d402f54d75f41949eba8be54236038cd5589f
yingh <yinghu.lu@gmail.com>
2022-05-09 21:25:26 -0700

    init saved game updates

--- modules/protocol/src/main/java/com/icodesoftware/Response.java
+++ modules/protocol/src/main/java/com/icodesoftware/Response.java
@@ -6,6 +6,13 @@ public interface Response extends Recoverable {
     int ACCESS_MODE_NOT_SUPPORTED = 3;
     int INSTANCE_FULL = 4;
 
+    String RESPONSE_SUCCESSFUL = "successful";
+    String RESPONSE_MESSAGE = "message";
+    String RESPONSE_CODE = "code";
+    String RESPONSE_COMMAND = "command";
+
+
+
     String command();
     void command(String command);
 
