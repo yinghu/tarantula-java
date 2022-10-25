2066210ac5acad36f5b042340daaad7cc09b8910
yinghulu <yinghu@perfectday.games>
2022-10-25 15:28:13 -0700

    service summary impl

--- platform/src/main/java/com/tarantula/admin/SudoRoleModule.java
+++ platform/src/main/java/com/tarantula/admin/SudoRoleModule.java
@@ -207,12 +207,12 @@ public class SudoRoleModule implements Module {
             m.add("metrics",ms);
             session.write(m.toString().getBytes());
         }
-        else if(session.action().equals("onEnableJVMMetrics")){
-            Metrics metrics = context.metrics(Metrics.PERFORMANCE);
-            long rt = (Integer.parseInt(session.name())*60000)/JVMMonitor.timerInternal;
-            JVMMonitor jvmMonitor = new JVMMonitor(this.context,metrics,rt);
-            this.context.schedule(jvmMonitor);
-            session.write(JsonUtil.toSimpleResponse(true,"scheduled with total checks ["+rt+"]").getBytes());
+        else if(session.action().equals("onEnableServiceView")){
+            //Metrics metrics = context.metrics(Metrics.PERFORMANCE);
+            //long rt = (Integer.parseInt(session.name())*60000)/JVMMonitor.timerInternal;
+            //JVMMonitor jvmMonitor = new JVMMonitor(this.context,metrics,rt);
+            //this.context.schedule(jvmMonitor);
+            session.write(JsonUtil.toSimpleResponse(true,"scheduled with total checks").getBytes());
         }
         else if(session.action().equals("onClusterList")){
             ClusterProvider.Summary summary = this.deploymentServiceProvider.clusterSummary();
