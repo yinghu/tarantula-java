c1454a51925d53b661c316da3f49614c6aff5787
yingh <yinghu.lu@gmail.com>
2022-10-28 11:14:01 -0700

    update startup sh

--- platform/src/main/bin/tarantula.sh
+++ platform/src/main/bin/tarantula.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 echo "Starting Tarantula Distribution System  ..."
-find ../lib  *.jar > lib.txt
-CP="../conf:../deploy:../web"
-while read line
-do
-V=`echo "${line}"`
-CP=${CP}:$V
-done < lib.txt
-rm -f lib.txt
+CP="../lib/*:../conf:../deploy:../web"
 JAVA_OPTS="$JAVA_OPTS -Xms1024m -Xmx2048m -XX:MaxDirectMemorySize=4096M -server"
 JAVA_HZ="--add-modules java.se --add-exports java.base/jdk.internal.ref=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.management/sun.management=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"
 java $JAVA_OPTS $JAVA_HZ -classpath $CP com.tarantula.platform.bootstrap.TarantulaMain
