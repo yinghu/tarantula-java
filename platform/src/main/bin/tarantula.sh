e858514fbdeee908cedc2aa2dae25b99436c17ba
yingh <yinghu.lu@gmail.com>
2022-10-31 22:17:18 -0700

    game service config hook

--- platform/src/main/bin/tarantula.sh
+++ platform/src/main/bin/tarantula.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting Tarantula Distribution System  ..."
 CP="../lib/*:../conf:../deploy:../web"
-JAVA_OPTS="$JAVA_OPTS -Xms4g -Xmx8g -XX:MaxDirectMemorySize=2g -server"
+JAVA_OPTS="$JAVA_OPTS -server -Xms12g -Xmx12g -XX:PermSize=512m -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70"
 JAVA_HZ="--add-modules java.se --add-exports java.base/jdk.internal.ref=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.management/sun.management=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"
 java $JAVA_OPTS $JAVA_HZ -classpath $CP com.tarantula.platform.bootstrap.TarantulaMain
