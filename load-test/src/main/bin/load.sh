08a5fa33cb72b5c2ca159dee366fae4760fa50ed
yingh <yinghu.lu@gmail.com>
2022-11-02 10:34:30 -0700

    replication config hook

--- load-test/src/main/bin/load.sh
+++ load-test/src/main/bin/load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting Tarantula Distribution System Load Test ..."
-CP="../lib/*"
-JAVA_OPTS="$JAVA_OPTS -Xms1024M -Xmx2048M -XX:MaxDirectMemorySize=512M -server"
+CP="../lib/*:bin"
+JAVA_OPTS="$JAVA_OPTS -Xms1024M -Xmx1024M -server"
 #echo "16384 65535" > /proc/sys/net/ipv4/ip_local_port_range
 java $JAVA_OPTS -classpath $CP com.tarantula.test.integration.Main
