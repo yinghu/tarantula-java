e0bb630870c224c98f21b3dc214ce1fe92246936
yingh <yinghu.lu@gmail.com>
2022-12-13 12:50:52 -0800

    removed node js module

--- websocket-server-nodejs/ws.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip >/etc/tarantula/ip.txt
-node ws.standalone.js
