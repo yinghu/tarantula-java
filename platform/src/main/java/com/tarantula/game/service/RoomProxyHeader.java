4e0868b403e0dc5591843fdd095b0d8e6c7c974d
yingh <yinghu.lu@gmail.com>
2023-02-01 09:40:06 -0800

    udp/lobby/room enhancement

--- platform/src/main/java/com/tarantula/game/service/RoomProxyHeader.java
+++ platform/src/main/java/com/tarantula/game/service/RoomProxyHeader.java
@@ -31,8 +31,8 @@ abstract public class RoomProxyHeader implements GameZone.RoomProxy {
     }
     @Override
     public byte[] update(Stub stub,byte[] payload){
-
-        return null;
+        this.context.log(new String(payload),OnLog.WARN);
+        return payload;
     }
 
     public byte[] update(Session session, Stub stub, byte[] payload){
