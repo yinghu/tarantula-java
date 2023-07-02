509bd1b1462d1b1d3c2f2c8ba40bcd4050cb6ae3
yingh <yinghu.lu@gmail.com>
2023-07-02 12:40:17 -0700

    save data enhancement

---
+++ platform/src/main/java/com/tarantula/game/module/PlayListModule.java
@@ -0,0 +1,26 @@
+package com.tarantula.game.module;
+
+import com.icodesoftware.Module;
+import com.icodesoftware.*;
+import com.tarantula.game.service.PlatformGameServiceProvider;
+
+public class PlayListModule implements Module{
+    private ApplicationContext context;
+    private PlatformGameServiceProvider gameServiceProvider;
+    @Override
+    public boolean onRequest(Session session, byte[] bytes) throws Exception {
+        throw new UnsupportedOperationException(session.action()+" not support");
+    }
+
+    @Override
+    public void setup(ApplicationContext applicationContext) throws Exception {
+        this.context = applicationContext;
+        this.gameServiceProvider = this.context.serviceProvider(context.descriptor().typeId());
+        if(this.descriptor().accessMode() == Access.PRIVATE_ACCESS_MODE) this.gameServiceProvider.exportServiceModule(this.context.descriptor().tag(),this);
+        this.context.log("Play list module started", OnLog.WARN);
+    }
+
+    public Descriptor descriptor(){
+        return this.context.descriptor();
+    }
+}
