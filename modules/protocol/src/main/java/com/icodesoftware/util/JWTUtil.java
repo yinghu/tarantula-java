8cca8890d7a496e6cdb94455cbb0eba4f5134da4
yingh <yinghu.lu@gmail.com>
2023-07-08 15:36:06 -0700

    added jwt util

--- modules/protocol/src/main/java/com/icodesoftware/util/JWTUtil.java
+++ modules/protocol/src/main/java/com/icodesoftware/util/JWTUtil.java
@@ -4,46 +4,103 @@ import com.google.gson.JsonObject;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.SecureRandom;
+import java.security.*;;
 import java.util.Base64;
 
 public class JWTUtil {
-    private final static String ALG = "HmacSHA256";
+    //Secret key algorithm
+    private final static String ALG_HMAC = "HmacSHA256";
+
+    //private public key pair algorithm RSA or ECDSA
+    private final static String ALG_RSA = "SHA1WithRSA";
+    //private final static String ALG_ECDSA = "ECDSA";
     private final static int KEY_SIZE = 32;
     private static SecureRandom secureRandom;
+
     static {
         secureRandom = new SecureRandom();
     }
 
-    public static JWT init(){
-        try{
+    public static JWT init() {
+        try {
             byte[] key = new byte[KEY_SIZE];
             secureRandom.nextBytes(key);
-            Mac mac = Mac.getInstance(ALG);
-            SecretKeySpec secretKey = new SecretKeySpec(key, ALG);
+            Mac mac = Mac.getInstance(ALG_HMAC);
+            SecretKeySpec secretKey = new SecretKeySpec(key, ALG_HMAC);
             mac.init(secretKey);
             return new JWT(mac);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static JWT init(byte[] key) {
+        try {
+            if (key.length < KEY_SIZE) throw new IllegalArgumentException("Key size should be more 32 bytes");
+            Mac mac = Mac.getInstance(ALG_HMAC);
+            SecretKeySpec secretKey = new SecretKeySpec(key, ALG_HMAC);
+            mac.init(secretKey);
+            return new JWT(mac);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static JWT init(PrivateKey privateKey) {
+        try{
+            Signature sig = Signature.getInstance(ALG_RSA);
+            sig.initSign(privateKey);
+            return new JWT(sig,true);
         }catch (Exception ex){
             throw new RuntimeException(ex);
         }
     }
 
+    public static JWT init(PublicKey publicKey) {
+        try{
+            Signature sig = Signature.getInstance(ALG_RSA);
+            sig.initVerify(publicKey);
+            return new JWT(sig,false);
+        }catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
     public static class JWT{
         private final Mac mac;
+        private final Signature signer;
+        private final Signature verifier;
+
+        private final String alg;
         public JWT(Mac mac){
             this.mac = mac;
+            this.signer = null ;
+            this.verifier = null;
+            this.alg = "HS256";
+        }
+
+        public JWT(Signature signature,boolean signing){
+            if(signing){
+                this.signer = signature;
+                this.verifier = null;
+            }
+            else{
+                this.signer = null;
+                this.verifier = signature;
+            }
+            this.alg = "RSA";
+            this.mac = null ;
         }
 
         public String token(OnProcess onProcess){
             try{
                 JsonObject header = new JsonObject();
-                header.addProperty("alg","HS256");
+                header.addProperty("alg",alg);
                 header.addProperty("typ","JWT");
                 JsonObject payload = new JsonObject();
                 if(!onProcess.process(header,payload)) throw new IllegalArgumentException("should be return true");
                 StringBuffer buffer = new StringBuffer(base64(header));
                 buffer.append(".").append(base64(payload));
-                String signature = Base64.getUrlEncoder().withoutPadding().encodeToString(mac.doFinal(buffer.toString().getBytes()));
+                String signature = sign(buffer.toString().getBytes(),new byte[0]);
                 return buffer.append(".").append(signature).toString();
             }catch (Exception ex){
                 throw new RuntimeException(ex);
@@ -55,19 +112,36 @@ public class JWTUtil {
                 String[] parts = token.split("\\.");
                 StringBuffer buffer = new StringBuffer(parts[0]);
                 buffer.append(".").append(parts[1]);
-                String signature = Base64.getUrlEncoder().withoutPadding().encodeToString(mac.doFinal(buffer.toString().getBytes()));
-                JsonObject h = JsonUtil.parse(Base64.getDecoder().decode(parts[0]));
-                JsonObject p = JsonUtil.parse(Base64.getDecoder().decode(parts[1]));
+                String signature = sign(buffer.toString().getBytes(),parts[2].getBytes());
+                JsonObject h = json(parts[0]);
+                JsonObject p = json(parts[1]);
                 return signature.equals(parts[2]) && onVerify.process(h,p);
             }catch (Exception ex){
                 return false;
             }
         }
 
+        private JsonObject json(String base64){
+            return JsonUtil.parse(Base64.getUrlDecoder().decode(base64));
+        }
         private String base64(JsonObject json){
-            return Base64.getEncoder().encodeToString(json.toString().getBytes());
+            return Base64.getUrlEncoder().encodeToString(json.toString().getBytes());
         }
 
+        private String sign(byte[] data,byte[] signature) throws Exception{
+            if(mac != null) return Base64.getUrlEncoder().withoutPadding().encodeToString(mac.doFinal(data));
+            if(signer!= null) {
+                signer.update(data);
+                return Base64.getUrlEncoder().withoutPadding().encodeToString(signer.sign());
+            }
+            if(verifier!=null){
+                verifier.update(data);
+                byte[] original = Base64.getUrlDecoder().decode(signature);
+                if(verifier.verify(original)) return Base64.getUrlEncoder().withoutPadding().encodeToString(original);
+                return "bad signature";
+            }
+            throw new IllegalArgumentException("no sign tool provided");
+        }
         public interface OnProcess{
             boolean process(JsonObject header,JsonObject payload);
         }
--- modules/protocol/src/test/java/com/icodesoftware/test/JWTTest.java
+++ modules/protocol/src/test/java/com/icodesoftware/test/JWTTest.java
@@ -5,6 +5,10 @@ import com.icodesoftware.util.TimeUtil;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
 import java.time.LocalDateTime;
 public class JWTTest {
 
@@ -27,5 +31,40 @@ public class JWTTest {
             p.get("aud").getAsString().equals("admin") && !TimeUtil.expired(TimeUtil.fromUTCMilliseconds(p.get("exp").getAsLong()))
         ));
     }
-
+    @Test(groups = { "JWT" })
+    public void simple2Test() {
+        byte[] key = "AIzaSyB52WeIzCm0F6UUJ7XkYNDoTlx7Xeu8DMA".getBytes();
+        JWTUtil.JWT jwt = JWTUtil.init(key);
+        String jwtToken = jwt.token((header,payload)->{
+            header.addProperty("kid","KID");
+            payload.addProperty("sub", "BDS/");
+            payload.addProperty("aud", "admin");
+            long expiry = TimeUtil.toUTCMilliseconds(LocalDateTime.now().plusHours(1));
+            payload.addProperty("exp",expiry);
+            return true;
+        });
+        Assert.assertTrue(jwt.verify(jwtToken,(h,p)->
+                p.get("aud").getAsString().equals("admin") && !TimeUtil.expired(TimeUtil.fromUTCMilliseconds(p.get("exp").getAsLong()))
+        ));
+    }
+    @Test(groups = { "JWT" })
+    public void simple3Test() throws Exception{
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(1024);
+        KeyPair keyPair = kpg.generateKeyPair();
+        PrivateKey privateKey = keyPair.getPrivate();
+        JWTUtil.JWT jwt = JWTUtil.init(privateKey);
+        String jwtToken = jwt.token((header,payload)->{
+            header.addProperty("kid","KID");
+            payload.addProperty("sub", "BDS/");
+            payload.addProperty("aud", "admin");
+            long expiry = TimeUtil.toUTCMilliseconds(LocalDateTime.now().plusHours(1));
+            payload.addProperty("exp",expiry);
+            return true;
+        });
+        JWTUtil.JWT pub = JWTUtil.init(keyPair.getPublic());
+        Assert.assertTrue(pub.verify(jwtToken,(h,p)->
+                p.get("aud").getAsString().equals("admin") && !TimeUtil.expired(TimeUtil.fromUTCMilliseconds(p.get("exp").getAsLong()))
+        ));
+    }
 }
