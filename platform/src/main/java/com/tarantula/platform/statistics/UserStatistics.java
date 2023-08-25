95d261127feb7a7b61fca42a5fa94272a1dbe112
yingh <yinghu.lu@gmail.com>
2023-08-24 23:18:06 -0700

    lmdb integration

--- platform/src/main/java/com/tarantula/platform/statistics/UserStatistics.java
+++ platform/src/main/java/com/tarantula/platform/statistics/UserStatistics.java
@@ -14,6 +14,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class UserStatistics extends RecoverableObject implements Statistics {
 
     private Map<String,StatisticsEntry> mappings = new ConcurrentHashMap<>();
+
     private Listener listener;
     public UserStatistics(){
         this.label = "Stats";
