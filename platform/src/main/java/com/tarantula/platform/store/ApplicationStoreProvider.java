4f30d7e858e91632e3ffbf3f882ac6692123cdf2
yingh <yinghu.lu@gmail.com>
2023-10-23 21:03:40 -0700

    transaction app store

--- platform/src/main/java/com/tarantula/platform/store/ApplicationStoreProvider.java
+++ platform/src/main/java/com/tarantula/platform/store/ApplicationStoreProvider.java
@@ -1,10 +1,12 @@
 package com.tarantula.platform.store;
 
-import com.icodesoftware.OnAccess;
-import com.icodesoftware.TarantulaLogger;
+import com.icodesoftware.*;
 import com.icodesoftware.logging.JDKLogger;
+import com.icodesoftware.service.ApplicationPreSetup;
 import com.icodesoftware.service.MetricsListener;
 import com.tarantula.game.service.PlatformGameServiceProvider;
+import com.tarantula.platform.GameCluster;
+import com.tarantula.platform.inventory.ApplicationRedeemer;
 import com.tarantula.platform.inventory.UserInventory;
 import com.tarantula.platform.service.AuthObject;
 
@@ -41,17 +43,26 @@ public class ApplicationStoreProvider extends AuthObject {
             logger.warn("IAP shopping item cannot be here");
             return false;
         }
-        long systemId = (long) params.get(OnAccess.SYSTEM_ID);
-
-        UserInventory inventory = this.platformGameServiceProvider.inventoryServiceProvider().inventory(systemId,shoppingItem.purchaseType().name(),shoppingItem.virtualCurrency().name());
-        if(inventory==null || !inventory.transact(shoppingItem.price()*(-1))){
-            logger.warn("Not enough balance to buy");
-            return false;
-        }
-        //String tid = UUID.randomUUID().toString();
-        //TransactionLog transaction = new TransactionLog(systemId,"soft purchase",bundleId);
-        //transaction.index(tid);
-        //serviceEventLogger.log(transaction);
+        String systemId = (String) params.get(OnAccess.SYSTEM_ID);
+        GameCluster gameCluster = platformGameServiceProvider.gameCluster();
+        Transaction t = gameCluster.transaction();
+        boolean suc = t.execute(ctx->{
+            ApplicationPreSetup setup =(ApplicationPreSetup)ctx;
+            Inventory inventory = setup.inventory(Long.parseLong(systemId), shoppingItem.virtualCurrency().name());
+            if(inventory==null || !inventory.transact(shoppingItem.price()*(-1))){
+                logger.warn("Not enough balance to buy");
+                return false;
+            }
+            Descriptor app = gameCluster.application(shoppingItem.configurationTypeId());
+            ApplicationRedeemer redeemer = new ApplicationRedeemer(systemId,setup);
+            redeemer.distributionKey(bundleId);
+            if(!setup.load(app,redeemer)) return false;
+            redeemer.redeem();
+            return true;
+        });
+        if(!suc) return false;
+        params.put(OnAccess.STORE_TRANSACTION_ID,serviceContext.distributionId());
+        params.put(OnAccess.STORE_QUANTITY,1);
         return true;
     }
 
