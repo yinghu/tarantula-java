fc61b5719b7c8bad22ba1ba7dbbf50f55a2fe729
yingh <yinghu.lu@gmail.com>
2023-12-10 23:32:12 -0800

    use jdk21

--- platform/src/main/bin/dev_tarantula.sh
+++ platform/src/main/bin/dev_tarantula.sh
@@ -3,4 +3,4 @@ echo "Starting Tarantula Distribution System  ..."
 CP="../lib/*:../conf:../deploy:../web"
 JAVA_OPTS="$JAVA_OPTS -Xms2g -Xmx2g -XX:+UseCompressedOops -server"
 JAVA_HZ="--add-modules java.se --add-exports java.base/jdk.internal.ref=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.management/sun.management=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"
-java $JAVA_OPTS $JAVA_HZ -classpath $CP com.tarantula.platform.bootstrap.TarantulaMain $1 $2 $3
+$JAVA_HOME/bin/java $JAVA_OPTS $JAVA_HZ -classpath $CP com.tarantula.platform.bootstrap.TarantulaMain $1 $2 $3
