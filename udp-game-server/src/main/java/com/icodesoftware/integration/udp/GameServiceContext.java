780b1c72c7e2b85c1ccfd34b505f5d69ce612167
yingh <yinghu.lu@gmail.com>
2024-01-06 13:19:44 -0800

    game play sync

--- udp-game-server/src/main/java/com/icodesoftware/integration/udp/GameServiceContext.java
+++ udp-game-server/src/main/java/com/icodesoftware/integration/udp/GameServiceContext.java
@@ -1,6 +1,7 @@
 package com.icodesoftware.integration.udp;
 
 import com.icodesoftware.*;
+import com.icodesoftware.logging.JDKLogger;
 import com.icodesoftware.protocol.GameContext;
 import com.icodesoftware.service.ApplicationSchema;
 import com.icodesoftware.service.TokenValidatorProvider;
@@ -8,6 +9,9 @@ import com.icodesoftware.service.TokenValidatorProvider;
 import java.util.concurrent.ScheduledFuture;
 
 public class GameServiceContext implements GameContext {
+
+    private TarantulaLogger logger = JDKLogger.getLogger(GameServiceContext.class);
+
     @Override
     public ScheduledFuture<?> schedule(SchedulingTask task) {
         return null;
@@ -15,12 +19,34 @@ public class GameServiceContext implements GameContext {
 
     @Override
     public void log(String message, int level) {
-
+        switch (level){
+            case OnLog.DEBUG:
+                this.logger.debug(message);
+                break;
+            case OnLog.INFO:
+                this.logger.info(message);
+                break;
+            case OnLog.WARN:
+                this.logger.warn(message);
+                break;
+        }
     }
 
     @Override
     public void log(String message, Exception error, int level) {
-
+        switch (level){
+            case OnLog.WARN:
+                if(error!=null){
+                    this.logger.warn(message);
+                }
+                else{
+                    this.logger.warn(message,error);
+                }
+                break;
+            case OnLog.ERROR:
+                this.logger.error(message,error);
+                break;
+        }
     }
 
     @Override
