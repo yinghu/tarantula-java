4456927540bc8fc60fc9fa857c6f941b0fa838b6
Harrison Beachey <harrison@perfectday.games>
2024-01-09 08:40:00 -0800

    fix some analytics message bugs

--- modules/earth8/src/main/java/com/perfectday/games/earth8/analytics/BattleEndTransaction.java
+++ modules/earth8/src/main/java/com/perfectday/games/earth8/analytics/BattleEndTransaction.java
@@ -2,6 +2,7 @@ package com.perfectday.games.earth8.analytics;
 
 import com.icodesoftware.Session;
 import com.icodesoftware.util.JsonUtil;
+import com.perfectday.games.earth8.BattleUpdate;
 
 public class BattleEndTransaction extends AnalyticsTransaction {
     private static final String MESSAGE_TYPE = "/earth8/player/0.0.1/battleEnd";
@@ -11,6 +12,10 @@ public class BattleEndTransaction extends AnalyticsTransaction {
         super(MESSAGE_TYPE, session);
         var object = JsonUtil.parse(clientData);
         data.addProperty("battle_id", battleId);
-        data.add("win", object.get("Win"));
+
+        var win = BattleUpdate.GetJsonBool(object, "Win", false);
+        data.addProperty("outcome", win ? "win" : "loss");
+        var fastSpeed = BattleUpdate.GetJsonBool(object,"EndedWithFastSpeed", false);
+        data.addProperty("battle_speed_x2", fastSpeed);
     }
 }
