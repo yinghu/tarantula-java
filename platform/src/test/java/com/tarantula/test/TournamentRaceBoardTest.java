74e67f968340d7ad5f5772095fb2deed8f8af4e3
yingh <yinghu.lu@gmail.com>
2024-01-19 22:37:12 -0800

    tournament race board updates

---
+++ platform/src/test/java/com/tarantula/test/TournamentRaceBoardTest.java
@@ -0,0 +1,35 @@
+package com.tarantula.test;
+
+
+import com.icodesoftware.DataStore;
+import com.tarantula.platform.OnSessionQuery;
+import com.tarantula.platform.PresenceIndex;
+import com.tarantula.platform.SessionIndex;
+import com.tarantula.platform.tournament.TournamentRaceBoard;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TournamentRaceBoardTest extends DataStoreHook{
+
+
+    @Test(groups = { "TournamentRaceBoard" })
+    public void tournamentRaceBoardTTest() {
+        DataStore dataStore = dataStoreProvider.createDataStore("test_tournament");
+        TournamentRaceBoard tournamentRaceBoard = new TournamentRaceBoard(10);
+        Assert.assertEquals(tournamentRaceBoard.size(),10);
+        //PresenceIndex presenceIndex = new PresenceIndex();
+        //presenceIndex.distributionId(presenceId);
+        //Assert.assertTrue(dataStore.createIfAbsent(presenceIndex,true));
+        //SessionIndex t1 = new SessionIndex();
+        //t1.ownerKey(presenceIndex.key());
+        //Assert.assertTrue(dataStore.create(t1));
+        //SessionIndex t2 = new SessionIndex();
+        //t2.ownerKey(presenceIndex.key());
+        //Assert.assertTrue(dataStore.create(t2));
+        //List<SessionIndex> tlist = dataStore.list(new OnSessionQuery(presenceIndex.key()));
+        //Assert.assertEquals(tlist.size(),2);
+    }
+
+}
