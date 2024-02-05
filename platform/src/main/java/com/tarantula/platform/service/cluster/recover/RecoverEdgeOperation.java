28eea9628228a0da60af1c9f06cbd4910d977123
yingh <yinghu.lu@gmail.com>
2024-02-04 22:05:05 -0800

    replication improvement

--- platform/src/main/java/com/tarantula/platform/service/cluster/recover/RecoverEdgeOperation.java
+++ platform/src/main/java/com/tarantula/platform/service/cluster/recover/RecoverEdgeOperation.java
@@ -32,7 +32,7 @@ public class RecoverEdgeOperation extends Operation {
 
     @Override
     public Object getResponse() {
-        return this.value.size()==0? null : this.value;
+        return this.value;
     }
 
     @Override
