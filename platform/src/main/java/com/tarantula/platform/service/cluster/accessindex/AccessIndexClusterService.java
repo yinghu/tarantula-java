2d06645c522b373c6199505edc76c317dd074a54
yingh <yinghu.lu@gmail.com>
2024-02-10 10:51:26 -0800

    repliaction updates

--- platform/src/main/java/com/tarantula/platform/service/cluster/accessindex/AccessIndexClusterService.java
+++ platform/src/main/java/com/tarantula/platform/service/cluster/accessindex/AccessIndexClusterService.java
@@ -95,7 +95,7 @@ public class AccessIndexClusterService implements ManagedService, RemoteService 
         TarantulaContext._integrationClusterStarted.await();
         this.deploymentServiceProvider = this.tarantulaContext.deploymentServiceProvider();
         this.tarantulaContext.clusterProvider().subscribe(MapStoreListener.INTEGRATION_MAP_STORE_NAME, event -> {
-            //if(event.source().equals(tarantulaContext.node().nodeName())) return false;
+            if(event.source().equals(tarantulaContext.node().nodeName())) return false;
             log.warn("Replicated on : "+tarantulaContext.node().nodeName()+" : "+event.source()+" : "+event.getClass().getName());
             if(event instanceof TransactionReplicationEvent){
                 tarantulaContext.onTransactionEvent(Distributable.INTEGRATION_SCOPE,(TransactionReplicationEvent)event);
