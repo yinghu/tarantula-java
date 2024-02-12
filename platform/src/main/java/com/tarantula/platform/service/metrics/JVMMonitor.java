3d6688e44f9a9da87a45a2d565056fcab4b92eea
yingh <yinghu.lu@gmail.com>
2024-02-11 23:19:28 -0800

    cluster operation updates

--- platform/src/main/java/com/tarantula/platform/service/metrics/JVMMonitor.java
+++ platform/src/main/java/com/tarantula/platform/service/metrics/JVMMonitor.java
@@ -3,7 +3,7 @@ package com.tarantula.platform.service.metrics;
 import com.icodesoftware.service.ServiceProvider;
 
 import java.lang.management.*;
-import java.util.HashMap;
+
 
 public class JVMMonitor implements ServiceProvider {
 
@@ -12,7 +12,7 @@ public class JVMMonitor implements ServiceProvider {
     private static MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
     private static ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
     private static  com.sun.management.OperatingSystemMXBean operatingSystemMXBean = (com.sun.management.OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
-    //private static final String THREAD_CPU_USAGE ="ThreadCPUUsage";
+
     private static final String PROCESS_CPU_USAGE ="ProcessCPUUsage";
 
     private static final String MEMORY_USAGE= "MemoryUsage";
@@ -25,7 +25,6 @@ public class JVMMonitor implements ServiceProvider {
         totalProcessors = operatingSystemMXBean.getAvailableProcessors();
     }
 
-    //private HashMap<Long,Long> ths = new HashMap<>();
     @Override
     public String name() {
         return NAME;
@@ -42,7 +41,6 @@ public class JVMMonitor implements ServiceProvider {
 
     @Override
     public void registerSummary(Summary summary){
-        //summary.registerCategory(THREAD_CPU_USAGE);
         summary.registerCategory(PROCESS_CPU_USAGE);
         summary.registerCategory(MEMORY_USAGE);
         summary.registerCategory(THREAD_COUNT);
@@ -52,32 +50,16 @@ public class JVMMonitor implements ServiceProvider {
     @Override
     public void updateSummary(Summary summary){
 
-        //long threadTimed = threadTimed();
         long processTimed = processTimed();
         long time = System.nanoTime();
         double elapsed = (time-lastUpTime)*totalProcessors;
         lastUpTime = time;
-        //double perThread = (threadTimed/elapsed)*100;
         double perProcess = (processTimed/elapsed)*100;
-        //summary.update(THREAD_CPU_USAGE,Double.parseDouble(String.format("%.2f",perThread)));
         summary.update(PROCESS_CPU_USAGE,Double.parseDouble(String.format("%.2f",perProcess)));
         summary.update(MEMORY_USAGE,memoryMXBean.getHeapMemoryUsage().getUsed()/1000000);
         summary.update(THREAD_COUNT,threadMXBean.getThreadCount());
     }
-    /**
-    private long threadTimed(){
-        long[] current = {0};
-        for(long tid : threadMXBean.getAllThreadIds()){
-            ths.compute(tid,(k,v)->{
-                long th = threadMXBean.getThreadCpuTime(tid);
-                if(th == -1) return null;
-                long last = v==null? 0 : v;
-                current[0] += (th-last);
-                return th;
-            });
-        }
-        return current[0];
-    }**/
+
     private long processTimed(){
         long cur = operatingSystemMXBean.getProcessCpuTime();
         long delta = cur-lastProcessTimed;
