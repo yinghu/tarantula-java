3c98f0d41cf7e61376aa001bf546f520d8c83a29
yingh <yinghu.lu@gmail.com>
2024-04-19 10:30:24 -0700

    migration api hook update

--- platform/src/main/java/com/tarantula/platform/service/persistence/DataMigrationListener.java
+++ platform/src/main/java/com/tarantula/platform/service/persistence/DataMigrationListener.java
@@ -1,12 +1,17 @@
 package com.tarantula.platform.service.persistence;
 
 import com.icodesoftware.DataStore;
+import com.icodesoftware.TarantulaLogger;
+import com.icodesoftware.lmdb.TransactionLogManager;
+import com.icodesoftware.logging.JDKLogger;
 import com.icodesoftware.service.DataStoreProvider;
 
 
 public class DataMigrationListener implements DataStoreProvider.MigrationListener {
 
-
+    private final static String data = "holee_service_presence_holee_lobby1";
+    private final static String index = TransactionLogManager.DATA_PREFIX_I+data;
+    private final static TarantulaLogger logger = JDKLogger.getLogger(DataMigrationListener.class);
     @Override
     public void migrate(DataStoreProvider dataStoreProvider) {
 
@@ -14,9 +19,12 @@ public class DataMigrationListener implements DataStoreProvider.MigrationListene
 
 
     public boolean migrate(DataStore dataStore){
-        if(!dataStore.name().equals("holee_service_presence_holee_lobby1")) return true;
-        dataStore.backup().drop(false);
-        return false;
+
+        if(dataStore.name().equals(data) || dataStore.name().equals(index)){
+            logger.warn("Truncating data from ["+dataStore.name()+"]");
+            dataStore.backup().drop(false);
+        }
+        return true;
     }
 
 }
