9a76fdba1c2eb6262c9993dfdd6d19637435f6a5
yingh <yinghu.lu@gmail.com>
2024-06-08 00:28:27 -0700

    hooked back configurable listener

--- platform/src/main/java/com/tarantula/game/module/PersonalDataStoreModule.java
+++ platform/src/main/java/com/tarantula/game/module/PersonalDataStoreModule.java
@@ -1,12 +1,14 @@
 package com.tarantula.game.module;
 
 import com.icodesoftware.ApplicationContext;
+import com.icodesoftware.Configurable;
 import com.icodesoftware.OnLog;
 import com.icodesoftware.Session;
 import com.icodesoftware.util.JsonUtil;
+import com.tarantula.platform.item.ConfigurableObject;
 
 
-public class PersonalDataStoreModule extends ModuleHeader{
+public class PersonalDataStoreModule extends ModuleHeader implements Configurable.Listener<ConfigurableObject> {
 
     @Override
     public boolean onRequest(Session session, byte[] bytes) throws Exception {
@@ -15,8 +17,9 @@ public class PersonalDataStoreModule extends ModuleHeader{
     }
 
     @Override
-    public void setup(ApplicationContext applicationContext) throws Exception {
-        super.setup(applicationContext);
+    public void setup(ApplicationContext context) throws Exception {
+        super.setup(context);
+        gameServiceProvider.configurationServiceProvider().registerConfigurableListener(context.descriptor(),this);
         this.context.log("Game personal data module started", OnLog.WARN);
     }
 
