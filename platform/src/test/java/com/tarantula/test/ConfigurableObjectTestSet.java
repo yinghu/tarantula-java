7cc64544ad26bd38dfb4b3cd7c4c12ad02b497c9
yingh <yinghu.lu@gmail.com>
2024-07-14 23:43:11 -0700

    buffer overfollow/ fallback to lz compressing/base64

--- platform/src/test/java/com/tarantula/test/ConfigurableObjectTestSet.java
+++ platform/src/test/java/com/tarantula/test/ConfigurableObjectTestSet.java
@@ -4,6 +4,10 @@ package com.tarantula.test;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import com.icodesoftware.Recoverable;
+import com.icodesoftware.lmdb.BufferProxy;
+import com.icodesoftware.lmdb.LocalHeader;
+import com.icodesoftware.util.Base64Util;
 import com.icodesoftware.util.CompressUtil;
 import com.icodesoftware.util.JsonUtil;
 import com.tarantula.game.service.GameObjectSetup;
@@ -106,18 +110,78 @@ public class ConfigurableObjectTestSet extends DataStoreHook{
     @Test(groups = { "ConfigurableObject" })
     public void sampleTournamentOverflow() {
         Exception exception = null;
+        Recoverable.DataBuffer buffer = BufferProxy.buffer(1000,true);
+        Recoverable.DataHeader h = new LocalHeader(100,1,10);
+        JsonObject[] header ={null};
+        JsonObject[] application ={null};
         try(InputStream in = (Thread.currentThread().getContextClassLoader().getResourceAsStream("sample-game-tournament-category-settings.json"))){
             JsonObject jsonObject = JsonUtil.parse(in);
             jsonObject.get("itemList").getAsJsonArray().forEach(e->{
                 JsonObject je = e.getAsJsonObject();
-                ByteBuffer buffer = ByteBuffer.allocate(1000);
-                buffer.put(je.toString().getBytes());
+                header[0] = je.get("header").getAsJsonObject();
+                application[0]=je.get("application").getAsJsonObject();
             });
 
         }catch (Exception ex){
             exception = ex;
         }
+        Assert.assertNull(exception);
+        try{
+            buffer.writeHeader(h);
+            buffer.writeUTF8(header[0].toString());
+            buffer.writeUTF8(application[0].toString());
+        }catch (Exception ex){
+            buffer.position(Recoverable.DataHeader.SIZE);
+            ByteBuffer src = ByteBuffer.allocateDirect(2000);
+            ByteBuffer dest = ByteBuffer.allocateDirect(1000);
+            CompressUtil.LZ4 lz4 = CompressUtil.lz4();
+            src.put(header[0].toString().getBytes());
+            src.flip();
+            lz4.compress(src,dest);
+            dest.flip();
+            byte[] comp = new byte[dest.remaining()];
+            dest.get(comp);
+            buffer.writeUTF8(Base64Util.toBase64String(comp));
+            src.clear();
+            dest.clear();
+            src.put(application[0].toString().getBytes());
+            src.flip();
+            lz4.compress(src,dest);
+            dest.flip();
+            byte[] comp1 = new byte[dest.remaining()];
+            dest.get(comp1);
+            buffer.writeUTF8(Base64Util.toBase64String(comp1));
+            exception = ex;
+        }
         Assert.assertNotNull(exception);
+        buffer.flip();
+        Recoverable.DataHeader hx = buffer.readHeader();
+        Assert.assertEquals(hx.revision(),h.revision());
+        Assert.assertEquals(hx.factoryId(),h.factoryId());
+        Assert.assertEquals(hx.classId(),h.classId());
+        byte[] ch = Base64Util.fromBase64String(buffer.readUTF8());
+        byte[] ca = Base64Util.fromBase64String(buffer.readUTF8());
+        CompressUtil.LZ4 lz4 = CompressUtil.lz4();
+        ByteBuffer src = ByteBuffer.allocateDirect(1000);
+        ByteBuffer dest = ByteBuffer.allocateDirect(2000);
+        src.put(ch);
+        src.flip();
+        lz4.decompress(src,dest);
+        dest.flip();
+        byte[] hp = new byte[dest.remaining()];
+        dest.get(hp);
+        JsonObject jh = JsonUtil.parse(hp);
+        Assert.assertEquals(jh.toString(),header[0].toString());
+        src.clear();
+        dest.clear();
+        src.put(ca);
+        src.flip();
+        lz4.decompress(src,dest);
+        dest.flip();
+        byte[] ha = new byte[dest.remaining()];
+        dest.get(ha);
+        JsonObject ja = JsonUtil.parse(ha);
+        Assert.assertEquals(ja.toString(),application[0].toString());
     }
 
 
