6ee408c34ac45b4b0317cf3d50a3fccf15941e6d
yinghulu <yinghu@perfectday.games>
2024-08-01 12:04:37 -0700

    fix low level code issue

--- platform/src/main/java/com/tarantula/platform/inbox/Mailbox.java
+++ platform/src/main/java/com/tarantula/platform/inbox/Mailbox.java
@@ -4,11 +4,12 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.icodesoftware.util.RecoverableObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Mailbox extends RecoverableObject {
 
-    public List<Announcement> announcementList;
+    public List<Announcement> announcementList = new ArrayList<>();
 
     @Override
     public JsonObject toJson() {
