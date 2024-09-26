93759df280174c60bb8e5b4452bbfd960ef2b96a
yinghulu <yinghu@perfectday.games>
2024-09-26 09:44:57 -0700

    merge master

--- platform/src/main/java/com/tarantula/platform/tournament/TournamentScanResponse.java
+++ platform/src/main/java/com/tarantula/platform/tournament/TournamentScanResponse.java
@@ -2,7 +2,8 @@ package com.tarantula.platform.tournament;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.tarantula.platform.ResponseHeader;
+import com.icodesoftware.util.ResponseHeader;
+
 
 import java.util.List;
 
