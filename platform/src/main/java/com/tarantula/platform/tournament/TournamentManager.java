be5f40dd4b874f61387822870e811839d2c103af
yinghulu <yinghu@perfectday.games>
2024-10-16 07:53:30 -0700

    tournament score on schedule

--- platform/src/main/java/com/tarantula/platform/tournament/TournamentManager.java
+++ platform/src/main/java/com/tarantula/platform/tournament/TournamentManager.java
@@ -653,8 +653,7 @@ public class TournamentManager extends RecoverableObject implements Tournament, 
         if(!global) return 0;
         TournamentInstance instance = lookupSegmentInstance(instanceId).tournamentInstance;
         //logger.warn(distributionId+" : "+instanceId+" : "+systemId+" : scored");
-        Transaction transaction = this.tournamentServiceProvider.gameCluster.transaction();
-        return instance.scoreSegmentSync(transaction,entryId,systemId,score);
+        return instance.scoreSegment(entryId,systemId,credits,score);
     }
 
 
