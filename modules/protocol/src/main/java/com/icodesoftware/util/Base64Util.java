14f2a652085bc5d8373534b4727847bebece2189
yingh <yinghu.lu@gmail.com>
2024-12-06 01:13:35 -0800

    etcd hook

--- modules/protocol/src/main/java/com/icodesoftware/util/Base64Util.java
+++ modules/protocol/src/main/java/com/icodesoftware/util/Base64Util.java
@@ -1,5 +1,6 @@
 package com.icodesoftware.util;
 
+import java.nio.ByteBuffer;
 import java.util.Base64;
 
 public class Base64Util {
@@ -10,4 +11,11 @@ public class Base64Util {
     public static byte[] fromBase64String(String data){
         return Base64.getDecoder().decode(data);
     }
+    public static String toBase64String(ByteBuffer buffer){
+        byte[] data = new byte[buffer.remaining()];
+        buffer.get(data);
+        return toBase64String(data);
+    }
+
+
 }
