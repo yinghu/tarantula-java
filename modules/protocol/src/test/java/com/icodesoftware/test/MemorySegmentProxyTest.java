d8ae2e2b24f3abe6821156d5a99217093d3816db
yingh <yinghu.lu@gmail.com>
2025-01-08 16:17:43 -0800

    sample segment on buffer

--- modules/protocol/src/test/java/com/icodesoftware/test/MemorySegmentProxyTest.java
+++ modules/protocol/src/test/java/com/icodesoftware/test/MemorySegmentProxyTest.java
@@ -1,8 +1,8 @@
 package com.icodesoftware.test;
 
 import com.icodesoftware.Recoverable;
+import com.icodesoftware.util.BufferProxy;
 import com.icodesoftware.util.LocalHeader;
-import com.icodesoftware.util.MemorySegmentProxy;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -21,20 +21,36 @@ public class MemorySegmentProxyTest {
     public void memoryProxyTest() {
         try(Arena arena = Arena.ofConfined()){
             MemorySegment memorySegment = arena.allocate(1000);
-            Recoverable.DataBuffer proxy = MemorySegmentProxy.memory(memorySegment);
+            Recoverable.DataBuffer proxy = BufferProxy.buffer(memorySegment.asByteBuffer());
             Recoverable.DataHeader h = new LocalHeader(10,1,3);
             proxy.writeHeader(h);
-            //proxy.writeShort((short)1);
+            proxy.writeShort((short)1);
             proxy.writeLong(100L);
             proxy.writeInt(5);
+            proxy.writeBoolean(true);
+            proxy.writeUTF8("hello");
             proxy.flip();
             Recoverable.DataHeader r = proxy.readHeader();
             Assert.assertEquals(h.revision(),r.revision());
             Assert.assertEquals(h.factoryId(),r.factoryId());
             Assert.assertEquals(h.classId(),r.classId());
-            //Assert.assertEquals(proxy.readShort(),1);
+            Assert.assertEquals(proxy.readShort(),1);
             Assert.assertEquals(proxy.readLong(),100L);
             Assert.assertEquals(proxy.readInt(),5);
+            Assert.assertTrue(proxy.readBoolean());
+            Assert.assertEquals(proxy.readUTF8(),"hello");
+            proxy.rewind();
+            Recoverable.DataHeader x = proxy.readHeader();
+            Assert.assertEquals(h.revision(),x.revision());
+            Assert.assertEquals(h.factoryId(),x.factoryId());
+            Assert.assertEquals(h.classId(),x.classId());
+            Assert.assertEquals(proxy.readShort(),1);
+            Assert.assertEquals(proxy.readLong(),100L);
+            Assert.assertEquals(proxy.readInt(),5);
+            Assert.assertTrue(proxy.readBoolean());
+            Assert.assertEquals(proxy.readUTF8(),"hello");
+            Assert.assertTrue(proxy.src().isDirect());
+
         }
 
     }
