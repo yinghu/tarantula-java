ff543d68cf91b74d1d9bd586b585bfb7879c7311
yingh <yinghu.lu@gmail.com>
2025-02-08 01:10:20 -0800

    trascation log re-api

--- modules/protocol/src/main/java/com/icodesoftware/Transaction.java
+++ modules/protocol/src/main/java/com/icodesoftware/Transaction.java
@@ -1,6 +1,7 @@
 package com.icodesoftware;
 
 import com.icodesoftware.service.Metadata;
+import com.icodesoftware.service.ServiceContext;
 
 import java.util.List;
 
@@ -25,6 +26,7 @@ public interface Transaction extends AutoCloseable{
         default void afterCommit(long transactionId){}
         default void afterAbort(long transactionId,Exception exception){}
     }
+
     interface Log{
         boolean deleting();
         int sourceScope();
@@ -50,15 +52,35 @@ public interface Transaction extends AutoCloseable{
          boolean read(Recoverable.DataBuffer buffer);
          boolean write(Recoverable.DataBuffer buffer);
     }
+
     interface LogManager{
+        void setup(ServiceContext serviceContext);
+
+        //callback on local transaction commit
         void onCommit(int scope, long transactionId);
+
+        //callback on local transaction abort
         void onAbort(int scope, long transactionId);
+
+        //local transaction logs per transaction
         List<Log> committed(int scope, long transactionId);
+
+        //callback on transaction replay
         void registerLogListener(Transaction.LogListener listener);
 
+        //local transaction replay per transaction
+        void onTransaction(List<Transaction.Log> transactionLogs);
+
+        //callback on store set
         void onUpdating(Metadata metadata, Recoverable.DataBuffer key, Recoverable.DataBuffer value, long transactionId);
+
+        //callback on store delete
         boolean onDeleting(Metadata metadata, Recoverable.DataBuffer key, Recoverable.DataBuffer value, long transactionId);
+
+        //callback on store load
+        boolean onRecovering(Metadata metadata,Recoverable.DataBuffer key,DataStore.BufferStream bufferStream);
     }
+
     interface LogListener {
         void onLog(Transaction.Log transactionLog);
     }
