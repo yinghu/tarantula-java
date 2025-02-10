eaa02becf8744f2cc9f96d1d589d1e466fcf9a1e
yinghulu <yinghu@perfectday.games>
2025-02-10 11:58:22 -0800

    elo rating hook

---
+++ platform/src/main/java/com/tarantula/platform/presence/pvp/PlayerEloRatingProxy.java
@@ -0,0 +1,49 @@
+package com.tarantula.platform.presence.pvp;
+
+import com.icodesoftware.Rating;
+import com.icodesoftware.util.RecoverableObject;
+import com.tarantula.game.SimpleStub;
+import com.tarantula.game.service.PlatformGameServiceProvider;
+
+public class PlayerRankingProxy extends RecoverableObject implements Rating {
+
+    private Rating playerRanking;
+    private PlatformGameServiceProvider platformGameServiceProvider;
+    public PlayerRankingProxy(Rating playerRanking,PlatformGameServiceProvider platformGameServiceProvider){
+        this.playerRanking = playerRanking;
+        this.platformGameServiceProvider = platformGameServiceProvider;
+    }
+
+    @Override
+    public int rank() {
+        return playerRanking.rank();
+    }
+
+    @Override
+    public int level() {
+        return playerRanking.level();
+    }
+
+    @Override
+    public double xp() {
+        return playerRanking.xp();
+    }
+
+    public void level(int eloAssigned){
+
+    }
+
+    @Override
+    public Rating update(double delta, double levelUpLimit) {
+        return playerRanking;
+    }
+
+    public Rating elo(boolean win,long opponentId,int teamPower){
+        DefenseTeam defenseTeam = this.platformGameServiceProvider.pvpBattleServiceProvider().defenseTeam(opponentId);
+        Rating opponentRaking = this.platformGameServiceProvider.presenceServiceProvider().rating(new SimpleStub(defenseTeam.playerId));
+        PVPPointGenerator.updateELO(playerRanking,opponentRaking,teamPower,defenseTeam.teamPower,win);
+        playerRanking.update();
+        opponentRaking.update();
+        return this;
+    }
+}
