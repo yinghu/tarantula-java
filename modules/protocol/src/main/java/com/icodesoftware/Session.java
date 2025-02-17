88cbfa128793a161bcc6189d4e2d5305f4d784bc
yingh <yinghu.lu@gmail.com>
2025-02-16 22:56:42 -0800

    code updates

--- modules/protocol/src/main/java/com/icodesoftware/Session.java
+++ modules/protocol/src/main/java/com/icodesoftware/Session.java
@@ -20,6 +20,8 @@ public interface Session extends OnApplication {
     String TARANTULA_MAGIC_KEY ="Tarantula-magic-key"; //the routing key
     String TARANTULA_TAG ="Tarantula-tag";
     String TARANTULA_TICKET ="Tarantula-ticket";
+    String TARANTULA_REFERENCE_ID ="Tarantula-reference-id";
+
     String TARANTULA_NAME ="Tarantula-name";
     String TARANTULA_CLIENT_ID ="Tarantula-client-id";
 
