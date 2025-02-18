8a5d2c9b5f638ac801d9b28fb4d87d4a90303923
ErikSagerPerfectDay <erik.sager@perfectday.games>
2025-02-18 16:57:27 -0600

    updated tests

--- platform/src/test/java/com/tarantula/test/ThirdPartyLoginTest.java
+++ platform/src/test/java/com/tarantula/test/ThirdPartyLoginTest.java
@@ -13,20 +13,24 @@ public class ThirdPartyLoginTest extends DataStoreHook{
     public void thirdPartyLoginTest() {
         String token = "fzzkU$DnJ$KPP3-(!7=5*+e5M%Dk,PhyG38n/g_nN-AJnq0X:6qR3j:B+K.;LS4Ye#$WY]cr{=%fVn=k;ZzLb!ptG,{#n=nNZFF}W1VWMB@%WtAX;dcA#(}cxF}v+AUxk.0uMB0UK&QBKjUP/R?%,5C*GA.He1$Fr2QP!xr&uCzS,9PFkza}K@c]gB=]::U}VBL[z?a197CjuWdiwD+zvBcaBtr&XLtB(";
 
+
         DataStore dataStore = dataStoreProvider.createDataStore(LoginProvider.DataStore);
         ThirdPartyLogin thirdPartyLogin = new ThirdPartyLogin("provider", SystemUtil.oid(),"SDGDGFDSG45236");
-        thirdPartyLogin.distributionKey("2345236235229643");
+        thirdPartyLogin.distributionId(serviceContext.distributionId());
         thirdPartyLogin.dataStore(dataStore);
-        dataStore.createIfAbsent(thirdPartyLogin,false);
+        Assert.assertTrue(dataStore.createIfAbsent(thirdPartyLogin,false));
 
         thirdPartyLogin.thirdPartyToken(token);
         thirdPartyLogin.update();
 
-        ThirdPartyLogin thirdPartyLoginLoaded = new ThirdPartyLogin();
-        thirdPartyLogin.distributionKey("2345236235229643");
-        thirdPartyLogin.dataStore(dataStore);
-        dataStore.load(thirdPartyLogin);
+        Assert.assertEquals(thirdPartyLogin.thirdPartyToken(), token);
 
-        Assert.assertEquals(token, thirdPartyLoginLoaded.thirdPartyToken());
+        ThirdPartyLogin thirdPartyLoginLoaded = new ThirdPartyLogin();
+        thirdPartyLoginLoaded.distributionId(thirdPartyLogin.distributionId());
+        thirdPartyLoginLoaded.dataStore(dataStore);
+        Assert.assertTrue(dataStore.load(thirdPartyLoginLoaded));
+
+        Assert.assertEquals(thirdPartyLoginLoaded.thirdPartyToken(), token);
     }
+}
 }
