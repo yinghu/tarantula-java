e45ed6797a8a0dd222f0bc4c0d87c5727451c367
ErikSagerPerfectDay <erik.sager@perfectday.games>
2025-02-19 16:39:44 -0600

    getClassId/ getFactoryId overided

--- platform/src/main/java/com/tarantula/platform/presence/pvp/DefenseCooldown.java
+++ platform/src/main/java/com/tarantula/platform/presence/pvp/DefenseCooldown.java
@@ -1,10 +1,20 @@
 package com.tarantula.platform.presence.pvp;
 
 import com.icodesoftware.util.RecoverableObject;
+import com.tarantula.platform.presence.PresencePortableRegistry;
 
 public class DefenseCooldown extends RecoverableObject {
 
     public long cooldownTimer;
+
+    @Override
+    public int getFactoryId() {
+        return PresencePortableRegistry.OID;
+    }
+    @Override
+    public int getClassId() {
+        return PresencePortableRegistry.DEFENSE_COOLDOWN_CID;
+    }
 
     @Override
     public boolean read(DataBuffer buffer) {
