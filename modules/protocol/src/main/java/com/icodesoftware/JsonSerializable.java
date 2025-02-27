dc1e551091aabaef0d0cbc60ae762e2728a39326
yingh <yinghu.lu@gmail.com>
2025-02-27 11:49:10 -0800

    json streaming api

--- modules/protocol/src/main/java/com/icodesoftware/JsonSerializable.java
+++ modules/protocol/src/main/java/com/icodesoftware/JsonSerializable.java
@@ -9,4 +9,5 @@ public interface JsonSerializable {
     default Map<String,Object> toMap(){ return null;}
     default void fromMap(Map<String,Object> properties){}
     JsonObject toJson();
+    default Recoverable.DataBuffer toDataBuffer(){ return null;}
 }
