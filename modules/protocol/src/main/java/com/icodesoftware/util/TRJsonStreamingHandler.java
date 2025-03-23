91ba6ecbd0e061ed9edb5506f9a17518ff7b501d
yingh <yinghu.lu@gmail.com>
2025-03-23 01:29:04 -0700

    json stream api

---
+++ modules/protocol/src/main/java/com/icodesoftware/util/TRJsonStreamingHandler.java
@@ -0,0 +1,46 @@
+package com.icodesoftware.util;
+
+import com.icodesoftware.JsonStreamingHandler;
+
+public class TRJsonStreamingHandler extends RecoverableObject implements JsonStreamingHandler {
+
+    @Override
+    public void onBeginObject(String tag, int index) {
+
+    }
+
+    @Override
+    public void onEndObject(String tag, int index) {
+
+    }
+
+    @Override
+    public void onBeginArray(String tag, int index) {
+
+    }
+
+    @Override
+    public void onEndArray(String tag, int index) {
+
+    }
+
+    @Override
+    public void onNumber(String tag, Number value, int index) {
+
+    }
+
+    @Override
+    public void onString(String tag, String value, int index) {
+
+    }
+
+    @Override
+    public void onBoolean(String tag, boolean value, int index) {
+
+    }
+
+    @Override
+    public void onNull(String tag, int index) {
+
+    }
+}
