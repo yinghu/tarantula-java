bbe9b3d6026d7237b9405d869e2b6687d5cc79b6
yingh <yinghu.lu@gmail.com>
2025-04-19 14:19:39 -0700

    ffm lmdb plugin

--- modules/lmdb/src/test/java/com/icodesoftware/lmdb/test/SmokeNativeTest.java
+++ modules/lmdb/src/test/java/com/icodesoftware/lmdb/test/SmokeNativeTest.java
@@ -2,6 +2,7 @@ package com.icodesoftware.lmdb.test;
 
 import com.icodesoftware.DataStore;
 import com.icodesoftware.Distributable;
+import com.icodesoftware.Transaction;
 import com.icodesoftware.util.SnowflakeKey;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -50,17 +51,55 @@ public class SmokeNativeTest extends LMDBHook{
     }
 
     @Test(groups = { "native data store" })
-    public void createEdgeTest(){
-        DataStore dataStore = lmdbDataStoreProvider.createDataStore("create_edge_test_object");
+    public void loadTest(){
+        DataStore dataStore = lmdbDataStoreProvider.createDataStore("load_test_object");
         TestObject testObject = new TestObject("atype","aname");
         Assert.assertTrue(dataStore.create(testObject));
         Assert.assertTrue(testObject.distributionId()>0);
         Assert.assertEquals(count(dataStore),1);
-        Assert.assertFalse(dataStore.createEdge(testObject,"link"));
-        testObject.ownerKey(SnowflakeKey.from(100));
-        testObject.onEdge(true);
-        Assert.assertTrue(dataStore.createEdge(testObject,"link"));
-        Assert.assertEquals(count(dataStore,SnowflakeKey.from(100),"link"),1);
+        TestObject load = new TestObject();
+        load.distributionId(testObject.distributionId());
+        Assert.assertTrue(dataStore.load(load));
+        Assert.assertEquals(load.name,testObject.name);
+        Assert.assertEquals(load.type,testObject.type);
+    }
+
+    @Test(groups = { "native data store" })
+    public void createEdgeTest(){
+        DataStore dataStore = lmdbDataStoreProvider.createDataStore("create_edge_test_object");
+        for(int i=0;i<10;i++){
+            TestObject testObject = new TestObject("atype","aname");
+            Assert.assertTrue(dataStore.create(testObject));
+            Assert.assertTrue(testObject.distributionId()>0);
+            Assert.assertFalse(dataStore.createEdge(testObject,"link"));
+            testObject.ownerKey(SnowflakeKey.from(100));
+            testObject.onEdge(true);
+            Assert.assertTrue(dataStore.createEdge(testObject,"link"));
+        }
+        Assert.assertEquals(count(dataStore),10);
+        Assert.assertEquals(count(dataStore,SnowflakeKey.from(100),"link"),10);
+    }
+
+    @Test(groups = { "native data store" })
+    public void transactionTest(){
+        try(Transaction transaction = lmdbDataStoreProvider.transaction(Distributable.DATA_SCOPE)){
+            transaction.execute(ctx->{
+                DataStore dataStore = ctx.onDataStore("transaction_test_object");
+                for(int i=0;i<10;i++){
+                    TestObject testObject = new TestObject("atype","aname");
+                    Assert.assertTrue(dataStore.create(testObject));
+                    Assert.assertTrue(testObject.distributionId()>0);
+                    Assert.assertFalse(dataStore.createEdge(testObject,"link"));
+                    testObject.ownerKey(SnowflakeKey.from(100));
+                    testObject.onEdge(true);
+                    Assert.assertTrue(dataStore.createEdge(testObject,"link"));
+                }
+                return true;
+            });
+        }
+        DataStore dataStore = lmdbDataStoreProvider.createDataStore("transaction_test_object");
+        Assert.assertEquals(count(dataStore),10);
+        Assert.assertEquals(count(dataStore,SnowflakeKey.from(100),"link"),10);
     }
 
 }
